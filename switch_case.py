6308dfe08f537bc0c65608e2d1747bd7cb560d76
Poojitha185 <poojithap741@gmail.com>
2026-03-10 22:41:58 +0530

    switch_case-Learning Basics

---
+++ switch_case.py
@@ -0,0 +1,19 @@
+days=["Monday","Tuesday","Wednesday","Thursday","Friday"]
+n=int(input("enter the day number: "))
+if n==1:
+    print(days[0])
+elif n==2:
+     print(days[1])
+elif n==3:
+     print(days[2])
+elif n==4:
+      print(days[3])
+elif n==5:
+      print(days[4])
+elif n==6:
+      print(days[5])
+elif n==7:
+      print(days[6])
+else:
+     print("invalid")
+
