b1cf0f4dbcdfa0e386605c9aefe1b9bca00e691f
Poojitha185 <poojithap741@gmail.com>
2026-03-11 23:57:43 +0530

    Day2-learning patterns

---
+++ LearningBasics/pattern_6.py
@@ -0,0 +1,6 @@
+for i in range(5,0,-1):
+    j=1
+    while(j<=i):
+      print(j,end="")
+      j=j+1
+    print()
---
+++ LearningBasics/pattern_7.py
@@ -0,0 +1,12 @@
+for i in range(1,6):
+    for j in range(5,i,-1):
+     print(" ",end="")
+    for k in range(0,2*i-1):
+        print("*",end="")
+    for l in range(5,i,-1):
+            print(" ",end="")
+    print()
+
+  
+
+
