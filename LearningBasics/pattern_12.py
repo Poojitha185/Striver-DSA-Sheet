073694772a8afd024a82402e04f51fe2d3c67d9c
Poojitha185 <poojithap741@gmail.com>
2026-03-12 22:27:42 +0530

    day3-learning patterns

---
+++ LearningBasics/pattern_12.py
@@ -0,0 +1,7 @@
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print(j, end="")
+    print(" " * (8 - 2*i), end="")
+    for k in range(i, 0, -1):
+        print(k, end="")
+    print()
--- LearningBasics/tempCodeRunnerFile.py
+++ LearningBasics/tempCodeRunnerFile.py
@@ -1,12 +1,2 @@
-for i in range(1,6):
-    if i%2!=0:
-      h=1
-    else:
-      h=0
-    for j in range(1,i+1):
-      print(h,end="")
-      if h==0:
-        h=1
-      else:
-        h=0
-    print()
+for i in range(1, 6):
+    print(" " * (5 - i) + "*" * (2 * i - 1))
