09c3e059d35a89a8264630a96c4e4bb970447db6
Poojitha185 <poojithap741@gmail.com>
2026-03-18 22:01:57 +0530

    day-8 solving know basic maths problem4(gcd of 2 nos euclidean algorithm)

---
+++ LearningBasics/gcd_or_hcf.py
@@ -0,0 +1,13 @@
+def gcd(a,b):
+    while a>0 and b>0:
+        if a>b:
+            a=a%b
+        else:
+            b=b%a
+    if a==0:
+     print("gcd of a 2 numbers: ",b)
+    else:
+     print("gcd of a 2 numbers: ",a)
+n1=int(input("enter  number1: "))
+n2=int(input("enter number2: "))
+gcd(n1,n2)
