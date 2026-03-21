61419814bc3a9a0684179eebc44e34ae04a04629
Poojitha185 <poojithap741@gmail.com>
2026-03-21 20:37:13 +0530

    Day10-Learn Basic Recursion problem3(using recursion approach)

--- LearningBasics/print_sumN_recursion.py
+++ LearningBasics/print_sumN_recursion.py
@@ -1,3 +1,5 @@
+#TC:O(N) because we are calling the function N times
+#SC:O(N) because of the recursive stack
 def sum(N,i,count):
     if i>N:
         return count
@@ -5,5 +7,5 @@ def sum(N,i,count):
      count=count+i
      return sum(N,i+1,count)
 N=int(input("enter the number: "))
-k=sum(N,1,0)
-print(k)
+print(sum(N,1,0))
+
