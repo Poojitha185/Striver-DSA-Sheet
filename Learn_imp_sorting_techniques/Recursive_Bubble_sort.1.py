a9c1fb9ad85a826ff24f727f8dc2dc0821bb2868
Poojitha185 <poojithap741@gmail.com>
2026-03-26 21:57:06 +0530

    Day16-Learn important sorting techniques-sorting2- problem2(using recursive_bubble_sort(using optimal approach) algorithm)

---
+++ Learn_imp_sorting_techniques/Recursive_Bubble_sort.1.py
@@ -0,0 +1,20 @@
+#Time Complexity: O(N2) for the worst and average cases and O(N) for the best case. Here, N = size of the array.
+#Space Complexity: O(N) auxiliary stack space.
+#using recursive function(optimal approach)
+
+def recursive_bubble_sort(arr,n):
+    if n==0:
+        return
+    didswap=0
+    for j in range(0,n-1):
+            if arr[j]>arr[j+1]:
+              arr[j],arr[j+1]=arr[j+1],arr[j]
+              didswap=1
+    if didswap==0:
+         return
+    recursive_bubble_sort(arr,n-1)
+arr=list(map(int,input("enter the arary: ").split(',')))
+n=len(arr)
+print("Before using bubble sort the arary: ",arr)
+recursive_bubble_sort(arr,n,)
+print("After the bubble sort the array: ",arr)
