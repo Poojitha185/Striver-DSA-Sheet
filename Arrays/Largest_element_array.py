18b73b800abb25f512880155a5cf8aa1bef77628
Poojitha185 <poojithap741@gmail.com>
2026-03-27 21:32:55 +0530

    Day17-solving problems on Arrays-problem1(using optimal approach)

--- Arrays/Largest_element_array.py
+++ Arrays/Largest_element_array.py
@@ -1,3 +1,5 @@
+#Time Complexity: O(N), where N is the size of the array, as we are iterating through the array once.
+#Space Complexity: O(1), as we are using a constant
 def largest_element(arr,n):
     large=arr[0]
     for i in range(n):
