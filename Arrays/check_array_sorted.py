0e370a8b4ec793bef0106330f76e372319eade2b
Poojitha185 <poojithap741@gmail.com>
2026-03-30 20:20:41 +0530

    Day20-solving problems on Arrays-problem3(using builtin function)

--- Arrays/check_array_sorted.py
+++ Arrays/check_array_sorted.py
@@ -1,7 +1,8 @@
 #TC:o(N logN),arr == sorted(arr) Compares element by element → O(N) and sorted(arr) sorts the array → O(N log N) time complexity. Overall, the time complexity is O(N log N) due to the sorting step.
 #SC:O(N), sorted(arr) creates a new sorted list, which takes O(N) space. The original array arr is not modified, so it does not contribute to additional space complexity.
+#using built-in function
 def check_arr_sorted(arr,n):
-    if arr==sorted(arr):              #sorted(arr) returns a new sorted list and arr.sort() returns None
+    if arr==sorted(arr):        #sorted(arr) returns a new sorted list and arr.sort() returns None
         return True
     else:
         return False
