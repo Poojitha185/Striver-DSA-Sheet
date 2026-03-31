9d86f1c766fae779416ba59b3c2121e64860f96a
Poojitha185 <poojithap741@gmail.com>
2026-03-31 22:32:51 +0530

    Day21-solving problems on Arrays-problem4(using brutal force approach and modified original array instead of creating new array)

--- Arrays/remove_duplicates_sortedarray.py
+++ Arrays/remove_duplicates_sortedarray.py
@@ -1,8 +1,15 @@
-#TC:O(N logN) because of the sorting step, which dominates the overall time complexity. The set() function runs in O(N) time. The sorted() function also runs in O(N log N) time. However, since the number of unique elements can be at most equal to the number of elements in the original array, we can consider it as O(N log N) in the worst case.
-#SC:O(N) because of the set() function, which creates a new set containing the unique elements from the original array. In the worst case, if all elements in the array are unique, the set will contain N elements, leading to O(N) space complexity. The sorted() function also creates a new list to store the sorted unique elements, which can also take O(N) space in the worst case. Therefore, the overall space complexity is O(N).
-#it creates new array but we have to modify original arary 
+def remove_duplicates(arr,n):
+    set_1=set()
+    index=0
+    for i in arr:
+        if i not in set_1:      #this condition is needed ,eventhough set_1 sores unique elemnents but duplicate element may be added arr[index]=i with this step set doenot control this
+         set_1.add(i)
+         arr[index]=i
+         index=index+1
+    return index
 arr=arr=list(map(int,input("enter the arary: ").split(',')))
-k=list(set(arr))
-print("The  number of unique elements in the array: ",len(k))
-print(sorted(k))
-      
+n=len(arr)
+k=remove_duplicates(arr,n)
+print("The number of unique elements are: ",k)
+print("array after removing elements are:", arr[:k])    #algorithm updates only first k elements which are unique
+
