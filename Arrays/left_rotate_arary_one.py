a6acf580e73e70bb3f6efea6dee92fade354f9fe
Poojitha185 <poojithap741@gmail.com>
2026-04-01 22:06:38 +0530

    Day22-solving problems on Arrays-problem5(using optimised approach)

--- Arrays/left_rotate_arary_one.py
+++ Arrays/left_rotate_arary_one.py
@@ -1,3 +1,6 @@
+#TC:O(N),  as we are iterating through the array once to perform the left rotation.
+#SC:O(1) bcz we are using a constant amount of extra space to store the first element of the array and not using extra space for new arary 
+#we are using given array in alogrithm to modify so sc is o(n) but when its comes to extra space sc is o(1)
 def left_rotate_one(arr,n):
     temp=arr[0]
     for i in range(1,n):
