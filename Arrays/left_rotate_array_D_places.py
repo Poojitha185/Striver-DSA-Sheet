29ddff900aae2799fd0540bfa672e54d2bca392b
Poojitha185 <poojithap741@gmail.com>
2026-04-02 21:00:01 +0530

    Day23-solving problems on Arrays-problem6(using brutal force approach)

--- Arrays/left_rotate_array_D_places.py
+++ Arrays/left_rotate_array_D_places.py
@@ -10,7 +10,7 @@ def left_rotate_D(arr,n,d):
     for i in range(n-d,n):
         arr[i]=temp[i-(n-d)]      #i-(n-d) or initialise j=0 and increment after each insert
     return arr
-arr=arr=list(map(int,input("enter the arary: ").split(',')))
+arr=list(map(int,input("enter the arary: ").split(',')))
 n=len(arr)
 d=int(input("enter no of places to rotate: " ))
 print("array before rotating by one: ",arr)
