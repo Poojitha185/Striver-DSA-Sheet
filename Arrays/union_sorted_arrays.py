660874cd3306128c66ed54d9b84e7960b35cb654
Poojitha185 <poojithap741@gmail.com>
2026-04-03 20:14:12 +0530

    Day24-solving problems on Arrays-problem9(using set method)

--- Arrays/union_sorted_arrays.py
+++ Arrays/union_sorted_arrays.py
@@ -1,3 +1,6 @@
+#Time Compleixty : O( (m+n)log(m+n) ) . Inserting an element in a set takes logN time, where N is no of elements in the set. At max set can store m+n elements {when there are no common elements and elements in arr,arr2 are distntict}. So Inserting m+n th element takes log(m+n) time. Upon approximation across inserting all elements in worst, it would take O((m+n)log(m+n) time.
+#Using HashSet also takes the same time, On average insertion in unordered_set takes O(1) time but sorting the union vector takes O((m+n)log(m+n))  time. Because at max union vector can have m+n elements.
+#Space Complexity : O(m+n) {If Space of Union ArrayList is considered} ,O(1) {If Space of union ArrayList is not considered}
 def union_sorted_arrays(arr1,arr2):
     k=set(arr1)|set(arr2)
     return sorted(k)
