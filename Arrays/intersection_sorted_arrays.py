42a0412a890d1ba5ceb39294ab36efa6ef57c930
Poojitha185 <poojithap741@gmail.com>
2026-04-04 22:28:04 +0530

    Day25-solving problems on Arrays-problem9(using brutal force approach)

---
+++ Arrays/intersection_sorted_arrays.py
@@ -0,0 +1,17 @@
+def intersection(arr1,arr2,n1,n2):
+    visited=[0]*n2
+    ans=[]
+    for i in range(n1):
+        for j in range(n2):
+            if arr1[i]==arr2[j] and visited[j]==0:
+                 ans.append(arr2[j])
+                 visited[j]=1
+                 break
+            if arr2[j]>arr1[i]:
+                break
+    return ans
+arr1=list(map(int,input("enter the first sorted arary: ").split(',')))
+arr2=list(map(int,input("enter the second sorted arary: ").split(',')))
+n1=len(arr1)
+n2=len(arr2)
+print("intersection of two sorted arrays: ",intersection(arr1,arr2,n1,n2))
