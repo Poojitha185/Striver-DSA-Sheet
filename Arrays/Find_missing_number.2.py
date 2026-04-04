d04c8df6d70442fb11c8b0408cf20d4f0138ffbc
Poojitha185 <poojithap741@gmail.com>
2026-04-04 23:42:36 +0530

    Day25-solving problems on Arrays-problem10(using optimal(sum) approach)

--- Arrays/Find_missing_number.2.py
+++ Arrays/Find_missing_number.2.py
@@ -1,7 +1,10 @@
+#TC:O(N),
+#SC:O(1), as we are not using any extra space to store the elements and modifying the original array in place.
+#using optimal 
 def missing_number(arr, n):
-    total = n * (n + 1) // 2
-    actual = sum(arr)
-    return total - actual
+    total = n * (n + 1) // 2  #tc:o(1),formula takes o(1) time
+    actual = sum(arr)         #tc:o(n) ,it internally runs a loop to calculate the sum of elements in the array
+    return total - actual     #tc:o(1),formula takes o(1) time
 arr = list(map(int, input("enter the first sorted arary: ").split(',')))
 n = len(arr)
 print("The missing number in the array is", missing_number(arr, n))
