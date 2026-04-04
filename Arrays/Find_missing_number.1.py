931b014a092ae22fb31d191fcb8103d18d6dcd8e
Poojitha185 <poojithap741@gmail.com>
2026-04-04 23:30:18 +0530

    Day25-solving problems on Arrays-problem10(using better(set) approach)

--- Arrays/Find_missing_number.1.py
+++ Arrays/Find_missing_number.1.py
@@ -1,5 +1,7 @@
+#TC:O(N), O(N)  (set creation)+ O(N × 1) (loop + constant check)= O(N)
+#SC:O(N), as we are using a set to store the elements of the array, which can take up to O(N) space in the worst case when all elements are distinct.
 def missing_number(arr, n):
-    s=set(arr)
+    s=set(arr)                 #To make searching fast (O(1)) Instead of checking in list (O(N))
     for i in range(1,n+1):
         if i not in s:
             return i
