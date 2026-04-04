fd6e16837c55b9a1145368afd75d8869ce3eefa3
Poojitha185 <poojithap741@gmail.com>
2026-04-04 23:19:23 +0530

    Day25-solving problems on Arrays-problem10(using brute force approach)

--- Arrays/Find_missing_number.py
+++ Arrays/Find_missing_number.py
@@ -1,6 +1,8 @@
+#TC:O(n**2)
+#SC=O(1), as we are not using any extra space to store the elements and modifying the original array in place.
 def missing_number(arr,n):
-    for i in range(1,n+1):
-        if i not in arr:
+    for i in range(1,n+1):  #tc:o(n)
+        if i not in arr:  #tc:o(n)
            return i
 
 arr=list(map(int,input("enter the first sorted arary: ").split(',')))
