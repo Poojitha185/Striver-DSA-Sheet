e4cca558d2b9907004f80ca4e9e56cc62559d958
Poojitha185 <poojithap741@gmail.com>
2026-04-06 22:28:27 +0530

    Day27-solving problems on Arrays-problem13(using brute force approach)

--- Arrays/Longest_subarray_sum.py
+++ Arrays/Longest_subarray_sum.py
@@ -1,15 +1,14 @@
-#TC~O(N*N*N)
+#TC~O(N*N)
 #SC:O(1), as we are not using any extra space.
 def longest_subarray_sum_k(arr,n,k):
     length=0
     #creating all possible subarrays and calculating their sum and comparing with k
-    for i in range(n):                 #tc:o(n) as we are iterating through the array once
+    for i in range(n):               #tc:o(n) as we are iterating through the array once
+        sum=0             
         for j in range(i,n):           #tc~o(n) as we are iterating through the array once for each element
-           sum=0
-           for l in range(i,j+1):      #tc~o(n), as we are iterating through the array once for each element in the subarray
-               sum=sum+arr[l]
-           if sum==k:
-            length=max(length,j-i+1) 
+            sum=sum+arr[j]             
+            if sum==k:
+             length=max(length,j-i+1) 
     return length
 arr=arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
