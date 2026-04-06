d8b72eae603b1c3e8f91dd3c548e65c39bdab0f7
Poojitha185 <poojithap741@gmail.com>
2026-04-06 19:40:49 +0530

    Day27-solving problems on Arrays-problem12(using brute force approach)

--- Arrays/Finding_number_appears_once.py
+++ Arrays/Finding_number_appears_once.py
@@ -1,6 +1,8 @@
+#TC:O(N*N)
+#SC:O(1), as we are not using any extra space 
 def number_once(arr):
-    for i in arr:               #It also repeats  work(counting) for duplicates
-        if arr.count(i)==1:
+    for i in arr:               #It also repeats  work(counting) for duplicates and tc:o(n), as we are iterating through the array once.
+        if arr.count(i)==1:        #tc:o(n) ,it internally runs a loop to count the number of occurrences of i in the array
          return i
 arr=arr=list(map(int,input("enter the arary: ").split(',')))
 print("The number that appears once in a array is: ",number_once(arr))
