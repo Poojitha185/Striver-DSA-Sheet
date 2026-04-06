90ad55ce05112f875341102084309c746be17ec7
Poojitha185 <poojithap741@gmail.com>
2026-04-06 19:55:38 +0530

    Day27-solving problems on Arrays-problem12(using better(dictionary) approach)

--- Arrays/Finding_number_appears_once.1.py
+++ Arrays/Finding_number_appears_once.1.py
@@ -1,8 +1,11 @@
+#TC:O(N), as we are iterating through the array once to create the frequency dictionary and then iterating through the frequency dictionary to find the number that appears once.
+#SC:O(N), as we are using a dictionary to store the frequency of each element in the array, which can take up to O(N) space in the worst case when all elements are distinct.
+#using Dictionary approach
 def finding_number_appears_once(arr):
-    freq={}
-    for i in arr:
+    freq={}             
+    for i in arr:          #tc:o(n)
         freq[i]=freq.get(i,0)+1
-    for j in freq:
+    for j in freq:         #tc:o(n)
         if freq[j]==1:
          return j
 arr=arr=list(map(int,input("enter the arary: ").split(',')))
