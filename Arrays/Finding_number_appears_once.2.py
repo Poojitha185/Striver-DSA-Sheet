eb62ef25925634b13f3dc7b782affcae25c92cdb
Poojitha185 <poojithap741@gmail.com>
2026-04-06 20:14:10 +0530

    Day27-solving problems on Arrays-problem12(using optimal(XOR) approach)

--- Arrays/Finding_number_appears_once.2.py
+++ Arrays/Finding_number_appears_once.2.py
@@ -1,6 +1,9 @@
+#TC:O(N)
+#SC:O(1)
+#using XOR(^) operator XOR of two same numbers is always 0 i.e. a ^ a = 0.XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.
 def finding_number_appears_once(arr):
     res=0
-    for i in arr:
+    for i in arr:        #tc:o(n)
         res=res^i   #^=XOR symbol
     return res
 arr=list(map(int,input("enter the arary: ").split(',')))
