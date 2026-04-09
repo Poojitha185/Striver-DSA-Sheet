b0e70199053e5be6feb92096fe1b29dbf67c0600
Poojitha185 <poojithap741@gmail.com>
2026-04-09 22:52:45 +0530

    Day30-solving problems on Arrays-problem15(using built in (sort) method)

--- Arrays/sort_0_1_2.py
+++ Arrays/sort_0_1_2.py
@@ -1,3 +1,5 @@
+#TC:O(n logn),log n levels of splitting n work per level
+#SC:O(N),Sorts in-place (modifies same array) But still uses extra memory internally. for Temporary arrays (for merging) and Storing runs (subarrays).
 def sort_0_1_2(arr):
     arr.sort()
     return arr
