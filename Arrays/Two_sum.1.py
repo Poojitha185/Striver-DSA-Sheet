9fd4bdf48fcddd2a41ad6af5d4bc1814eb594106
Poojitha185 <poojithap741@gmail.com>
2026-04-09 19:58:58 +0530

    Day30-solving problems on Arrays-problem14(using better(dictionary) approach)

--- Arrays/Two_sum.1.py
+++ Arrays/Two_sum.1.py
@@ -1,3 +1,5 @@
+#Time Complexity: O(N) because we traverse the array only once, and each lookup or insertion in the hash map takes O(1) on average, where N is the size of the array.
+#Space Complexity: O(N) since in the worst case we may store all elements of the array in the hash map.
 def two_sum(arr,n,target):
    seen={}
    for i,num in enumerate(arr):
