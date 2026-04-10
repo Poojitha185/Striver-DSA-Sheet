782652482c824f775cc8bda682ea39d64dc382ac
Poojitha185 <poojithap741@gmail.com>
2026-04-10 22:42:46 +0530

    Day31-solving problems on Arrays-problem15(using built in (sort) method)

--- Arrays/sort_0_1_2.py
+++ Arrays/sort_0_1_2.py
@@ -4,4 +4,4 @@ def sort_0_1_2(arr):
     arr.sort()
     return arr
 arr=list(map(int,input("enter the array: ").split(',')))
-print("the array after sorting: ",sort_0_1_2(arr))
+print("The array after sorting the elements: ",sort_0_1_2(arr))
