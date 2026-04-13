a2354236317b03b4f823841e85f3fb1a81be6d51
Poojitha185 <poojithap741@gmail.com>
2026-04-13 21:20:06 +0530

    Day34-solving problems on Arrays-problem16(using optimal(Boyer-Moore Voting Algorithm) approach)

--- Arrays/majority_element-1_2.py
+++ Arrays/majority_element-1_2.py
@@ -1,7 +1,12 @@
+#Time Complexity: O(N), where N is the size of the input array. This is because we are iterating through the array once to find the potential majority element and then again to verify it.
+
+#Space Complexity: O(1), as we are using only a constant amount of extra space.
+
+#Boyer-Moore Voting Algorithm to find the majority element in an array.
 def majority_element(arr,n):
     ele=0
     cnt=0
-    for num in arr:
+    for num in arr:   # Boyer-Moore Voting Algorithm to find a potential majority element.tc:o(n)
             if cnt == 0:
                 cnt = 1
                 ele = num
@@ -9,7 +14,7 @@ def majority_element(arr,n):
                 cnt += 1
             else:
                 cnt -= 1
-    count1=arr.count(ele)
+    count1=arr.count(ele) #counting the occurrences of the potential majority element.tc:o(n)
     if count1>n//2:
          return ele
     return -1
