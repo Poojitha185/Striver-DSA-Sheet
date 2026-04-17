45e1bde7df3502ddb9c8f38a99350dfc3039f005
Poojitha185 <poojithap741@gmail.com>
2026-04-17 21:17:48 +0530

    Day38-solving problems on Arrays-problem17(using optimal approach)

--- Arrays/maximum_subarray_sum.2.py
+++ Arrays/maximum_subarray_sum.2.py
@@ -6,13 +6,13 @@
 
 def maximum_subarray(arr,n):
      sum=0
-     maxi=arr[0]
+     maximum=arr[0]
      for i in range(n):
           sum+=arr[i]
-          maxi=max(maxi,sum)
+          maximum=max(maximum,sum)
           if sum<0:
             sum=0
-     return maxi
+     return maximum
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
 print("The maximum subarray sum is:",maximum_subarray(arr,n))
