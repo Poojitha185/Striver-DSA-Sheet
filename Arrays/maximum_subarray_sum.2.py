8dac599cad68b5cdfd8594451beca15d62815140
Poojitha185 <poojithap741@gmail.com>
2026-04-17 20:37:10 +0530

    Day37-solving problems on Arrays-problem17(using optimal approach)

--- Arrays/maximum_subarray_sum.2.py
+++ Arrays/maximum_subarray_sum.2.py
@@ -1,3 +1,9 @@
+#Time Complexity: O(n), where n is the number of elements in the array. We traverse the array only once.
+
+#Space Complexity: O(1). We use a constant amount of space for variables.
+
+#maximum subarray sum is entire array sum but when we find sum of array it will give wrong answer when array contain negative numbers so we will reset sum to 0 when it becomes negative because negative sum will decrease the overall sum of the subarray. This way, we can ensure that we are always considering the maximum possible sum for the subarray.
+
 def maximum_subarray(arr,n):
      sum=0
      maxi=arr[0]
