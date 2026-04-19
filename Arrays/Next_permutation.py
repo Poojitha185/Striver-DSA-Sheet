5a21262c754ad3e6ee75b327fa97175ba9272184
Poojitha185 <poojithap741@gmail.com>
2026-04-19 21:50:40 +0530

    Day40-solving problems on Arrays-problem19(using brutal force  approach)

--- Arrays/Next_permutation.py
+++ Arrays/Next_permutation.py
@@ -1,7 +1,7 @@
 #Time Complexity: O(N!*N), since we are generating all possible permutations, it takes N! time.
 #Space Complexity: O(N!), storing all permutations.
 
-from itertools import permutations
+from itertools import permutations         #The itertools contain permutations function is a built-in function in Python that generates all possible permutations of a given iterable (like a list or string). It returns an iterator that produces tuples of the permutations. In this code, we use it to generate all permutations of the input array, which we then sort and compare to find the next permutation.
 def next_permutation(arr,n):
     perm=sorted(set(permutations(arr)))    #permutations(n) generates all possible permutations of the input array and give list of tupeles containing a permutation of the array and to delete duplicates we use set and pass set to sorted to sort the list of tuples and answer will return in list format 
     current=tuple(arr)                     #arr is converted to tuple to compare the tuples in p[ermuatations list]
