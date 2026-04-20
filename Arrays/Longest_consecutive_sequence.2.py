12f3ccdb05fff95932206ed22e9ebc20ab652936
Poojitha185 <poojithap741@gmail.com>
2026-04-20 22:16:26 +0530

    Day41-solving problems on Arrays-problem20(using optimal(using set) approach)

--- Arrays/Longest_consecutive_sequence.2.py
+++ Arrays/Longest_consecutive_sequence.2.py
@@ -1,3 +1,7 @@
+#Time Complexity: O(n), where n is the number of elements in the array. This is because we traverse each element once to build the set and then again to find the longest consecutive sequence.
+
+#Space Complexity: O(n), as we use a set to store the unique elements of the array, which in the worst case can be equal to the size of the input array.
+
 def longest_consecutive_sequence(arr,n):
     if n==0:
         return 0
