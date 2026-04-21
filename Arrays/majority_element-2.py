33e58de40db3858d01ebc8211f6a6ac20f57f18e
Poojitha185 <poojithap741@gmail.com>
2026-04-21 22:16:21 +0530

    Day42-solving problems on Arrays-problem22(using brute force approach)

--- Arrays/majority_element-2.py
+++ Arrays/majority_element-2.py
@@ -1,14 +1,16 @@
-#Time Complexity: O(N), where N is the size of the input array. This is because we are iterating through the array once to count occurrences and then iterating through the dictionary to find the majority element.
-#Space Complexity: O(N), as we are using a dictionaryto store the counts of each element, which can take up to N space in the worst case.
-def find_majority_element(arr,n):
-    dic={}
-    for i in arr:     #tc:o(n)
-        dic[i]=dic.get(i,0)+1       
-    k=max(dic,key=dic.get)    #it runs loop so tc:o(n)
-    v=dic[k]
-    if v>n//2:
-        return k,v
+#TC:O(n^2), as we are using nested loops to count the occurrences of each element in the array.
+#SC:O(n), as we are using a set to store the unique elements of the array, which in the worst case can be equal to the size of the input array.
+def majority_element(arr,n):
+  s=set(arr)
+  result=[]
+  for i in s:
+    count=0
+    for j in range(n):
+      if arr[j]==i:
+        count+=1
+    if count >(n//3):
+       result.append(i)
+  return result
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
-w,x=find_majority_element(arr,n)
-print("The number",w,"appears",x,"times in",n,"sized array")
+print("The majority element is: ",majority_element(arr,n))
