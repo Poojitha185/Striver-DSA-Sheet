1ff8ebe776221ed1adfeb00305af84af0a065a32
Poojitha185 <poojithap741@gmail.com>
2026-04-22 21:43:17 +0530

    Day43-solving problems on Arrays-problem22(using better approach)

--- Arrays/majority_element-2.2.py
+++ Arrays/majority_element-2.2.py
@@ -1,3 +1,5 @@
+#TC:O(n), as we traverse the array once to build the dictionary and then again to find the majority elements.
+#SC:O(n), as we are using a dictionary to store the count of each element in the array, which in the worst case can be equal to the size of the input array.
 def majority_element(arr,n):
     dic={}
     result=[]
@@ -9,6 +11,7 @@ def majority_element(arr,n):
         if len(result)==2:
             break
     return result
+
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
 print("The majority element is: ",majority_element(arr,n))
