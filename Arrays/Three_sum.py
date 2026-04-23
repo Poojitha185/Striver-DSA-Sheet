c4be6452046674ea1c97ba341a827e43806f7d4d
Poojitha185 <poojithap741@gmail.com>
2026-04-23 20:59:27 +0530

    Day44-solving problems on Arrays-problem23(using brute force approach)

--- Arrays/Three_sum.py
+++ Arrays/Three_sum.py
@@ -1,3 +1,5 @@
+#Time Complexity: O(n^3), where n is the number of elements in the array. This is because we have three nested loops to check all possible triplets.
+#Space Complexity: O(n^2), as we are using a set to store unique triplets and list to store the final result
 def three_sum(arr,n):
     result=set()
     for i in range(n):
