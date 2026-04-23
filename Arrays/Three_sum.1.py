9fc59f95cbf7fd0042c10c73b499f64579250715
Poojitha185 <poojithap741@gmail.com>
2026-04-23 22:39:11 +0530

    Day44-solving problems on Arrays-problem23(using better approach)

--- Arrays/Three_sum.1.py
+++ Arrays/Three_sum.1.py
@@ -1,3 +1,5 @@
+#TC:O(n^2), as we are using nested loops to check for each pair of elements in the array and then checking if the third element that sums to zero exists in the set.
+#SC:O(n^2), as we are using a set to store unique triplets and list to store the final result
 def three_sum(arr,n):
     result=set()
     for i in range(n):
@@ -5,9 +7,9 @@ def three_sum(arr,n):
         for j in range(i+1,n):
             last=-(arr[i]+arr[j])
             if last in s:
-                result.add(tuple(sorted([arr[i],arr[j],last])))
+                result.add(tuple(sorted([arr[i],arr[j],last])))    # use tuple when list is getting add to set and we use sort to avoid duplicate triplets in different order
             s.add(arr[j])
-    final=[list(x) for x in result]
+    final=[list(x) for x in result]                                #return in same datastructure as input
     return final
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
