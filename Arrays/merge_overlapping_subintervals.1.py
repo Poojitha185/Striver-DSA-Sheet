4ef7d9612d7cb049881320bff533302e9bb28e45
Poojitha185 <poojithap741@gmail.com>
2026-04-26 22:06:09 +0530

    Day47-solving problems on Arrays-problem24(using optimal approach)

---
+++ Arrays/merge_overlapping_subintervals.1.py
@@ -0,0 +1,13 @@
+def merge_overlapping_subintervals(arr,n):
+     arr.sort()
+     merged=[]
+     for i in arr:
+          if not merged or merged[-1][1]<i[0]:
+               merged.append(i)
+          else:
+               merged[-1][1] = max( merged[-1][1], i[1])
+     return merged
+arr = input("enter the array: ")
+arr = [list(map(int, x.strip('[]').split(','))) for x in arr.split('],')] 
+n=len(arr)
+print("array after merging overlapping subintervals: ",merge_overlapping_subintervals(arr,n))
