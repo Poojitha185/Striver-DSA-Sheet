5036ae1639fd969e6148ad5dadd1d3d466ed7ab4
Poojitha185 <poojithap741@gmail.com>
2026-04-28 21:04:56 +0530

    Day49-solving problems on Arrays-problem26(using brute force (buit-in functions) approach )

--- Arrays/Find_repeating_and_missing_numbers.py
+++ Arrays/Find_repeating_and_missing_numbers.py
@@ -1,12 +1,12 @@
-
-
+#TC:O(N^2), as we are iterating through the array once to check for each number from 1 to n if it is repeating or missing, and the count() method itself takes O(N) time.
+#SC:O(1), as we are not using any extra space to store the frequency of elements or any other data structure, we are just using two variables to store the repeating and missing numbers.
 def repeating_and_missing_numbers(arr,n):
     repeating=0
     missing=0
     for i in range(1,n+1):
-        if arr.count(i)>1:
+        if arr.count(i)>1:    #tc:O(n) 
             repeating=i
-        elif i not in arr:
+        elif i not in arr:    #tc:O(n)
             missing=i
     return [repeating, missing]
 
