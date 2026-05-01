8c4aa1fc02f2a0e317701818891c1a0f606d7775
Poojitha185 <poojithap741@gmail.com>
2026-05-01 20:07:14 +0530

    Day52-solving problems on Arrays-problem27(using brute force approach)

---
+++ Arrays/maximum_product_subarray_array.py
@@ -0,0 +1,11 @@
+def max_product_subarray(arr,n):
+    res=arr[0]                          # This method is appropriate for array containing negative values ,fif there are only  positive values in array ,then set to 1
+    for i in range(n):
+        product=1
+        for j in range(i,n):
+            product=product*arr[j]
+        res=max(res,product)
+    return res
+arr=list(map(int,input("enter the array: ").split(',')))
+n=len(arr)
+print("The maximum product subarray is:",max_product_subarray(arr,n))
