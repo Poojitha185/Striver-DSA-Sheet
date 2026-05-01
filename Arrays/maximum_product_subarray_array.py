91cfa0f8e8d7b9dd2fc0d2ab5520ba22b9e5f9f7
Poojitha185 <poojithap741@gmail.com>
2026-05-01 20:08:13 +0530

    Day52-solving problems on Arrays-problem27(using brute force approach)

--- Arrays/maximum_product_subarray_array.py
+++ Arrays/maximum_product_subarray_array.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(N^2), we check the product of all possible subarrays using two nested loops.
+#Space Complexity: O(1), No extra space is used.
+
 def max_product_subarray(arr,n):
     res=arr[0]                          # This method is appropriate for array containing negative values ,fif there are only  positive values in array ,then set to 1
     for i in range(n):
