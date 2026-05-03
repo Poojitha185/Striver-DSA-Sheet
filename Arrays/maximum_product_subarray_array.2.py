52b177f05c495c442da8410bd199cfc70a9fb917
Poojitha185 <poojithap741@gmail.com>
2026-05-03 22:23:58 +0530

    Day54-solving problems on Arrays-problem27(using optimal approach)

--- Arrays/maximum_product_subarray_array.2.py
+++ Arrays/maximum_product_subarray_array.2.py
@@ -1,5 +1,4 @@
-#Time Complexity: O(N), every element of array is visited once.
-#Space Complexity: O(1) , only constant variables are used.
+
 
 class Solution:
     # This function returns the maximum product
