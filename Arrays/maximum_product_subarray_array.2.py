5b39af651a85fde47d93667c1ccfeea14973602e
Poojitha185 <poojithap741@gmail.com>
2026-05-04 21:45:07 +0530

    Day55-solving problems on Arrays-problem27(using optimal approach)

--- Arrays/maximum_product_subarray_array.2.py
+++ Arrays/maximum_product_subarray_array.2.py
@@ -1,31 +1,20 @@
 #Time Complexity: O(N), every element of array is visited once.
 #Space Complexity: O(1) , only constant variables are used.
 
-class Solution:
-    # This function returns the maximum product
-    # of any contiguous subarray using optimized approach
-    def maxProduct(self, nums):
-        res = nums[0]
-        maxProd = nums[0]
-        minProd = nums[0]
+def max_product_subarray(arr):
+    curr_max = curr_min = ans = arr[0]
 
-        # Traverse from second element
-        for i in range(1, len(nums)):
-            curr = nums[i]
+    for i in range(1, len(arr)):
+        x = arr[i]
 
-            # Swap max and min if current is negative
-            if curr < 0:
-                maxProd, minProd = minProd, maxProd
+        if x < 0:
+            curr_max, curr_min = curr_min, curr_max
 
-            # Update max and min product
-            maxProd = max(curr, maxProd * curr)
-            minProd = min(curr, minProd * curr)
+        curr_max = max(x, curr_max * x)
+        curr_min = min(x, curr_min * x)
 
-            # Update result
-            res = max(res, maxProd)
+        ans = max(ans, curr_max)
 
-        return res
-
-nums = [2, 3, -2, 4]
-sol = Solution()
-print(sol.maxProduct(nums))
+    return ans
+arr = list(map(int, input("enter the array: ").split(',')))
+print("The maximum product subarray is:", max_product_subarray(arr))
