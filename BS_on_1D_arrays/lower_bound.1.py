f73acf505587b8648eb58686d8aaca2a516deaf7
Poojitha185 <poojithap741@gmail.com>
2026-05-06 19:29:31 +0530

    Day57-solving problems on BS on 1D arrays-problem2(using optimal force(binary search) approach)

---
+++ BS_on_1D_arrays/lower_bound.1.py
@@ -0,0 +1,16 @@
+def lower_bound(arr,x,n):
+    low=0
+    high=n-1
+    ans=n
+    while(low<=high):
+        mid=(low+high)//2
+        if arr[mid]>=x:
+            ans=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ans
+arr=list(map(int,input("enter the array: ").split(',')))
+x=int(input("enter the target: "))
+n=len(arr)
+print("the target is found at index:",lower_bound(arr,x,n))
