2f8e21ba3d2d677f3f64cd49bc68c95b97da5ab6
Poojitha185 <poojithap741@gmail.com>
2026-05-06 18:57:00 +0530

    Day57-solving problems on BS on 1D arrays-problem2(using Brutal force(linear serach) approach)

---
+++ BS_on_1D_arrays/lower_bound.py
@@ -0,0 +1,9 @@
+def lower_bound(arr,n,x):
+    for i in range(n):
+        if arr[i]>=x:
+            return i
+    return n
+arr=list(map(int,input("enter the array: ").split(',')))
+x=int(input("enter the target: "))
+n=len(arr)
+print("the target is found at index:",lower_bound(arr,n,x))
