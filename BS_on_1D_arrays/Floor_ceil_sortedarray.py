de61f15df5806019c391c57fcc4686321b5bc15a
Poojitha185 <poojithap741@gmail.com>
2026-05-08 22:35:15 +0530

    Day59-solving problems on BS on 1D arrays-problem5(using binary search approach)

---
+++ BS_on_1D_arrays/Floor_ceil_sortedarray.py
@@ -0,0 +1,29 @@
+def floor(arr,n,k):
+    low=0
+    high=n-1
+    floor =-1
+    while low <= high:
+        mid=(low+high)//2
+        if arr[mid]<=k:
+            floor=arr[mid]
+            low=mid+1
+        else:
+            high=mid-1
+    return floor
+def ceil(arr,n,k):
+    low=0
+    high=n-1
+    ceil=-1
+    while low <= high:
+        mid=(low+high)//2
+        if arr[mid]>=k:
+            ceil=arr[mid]
+            high=mid-1
+        else:
+            low=mid+1
+    return ceil
+arr=list(map(int,input("enter the array: ").split(',')))
+k=int(input("enter the target: "))
+n=len(arr)
+floor,ceil=floor(arr,n,k),ceil(arr,n,k)
+print("the floor and ceil of the target is found at index:",floor,ceil)
