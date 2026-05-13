7c67c6b5e316a8f19394519d46e1718296c83b57
Poojitha185 <poojithap741@gmail.com>
2026-05-13 19:31:07 +0530

    Day64-solving problems on BS on 1D arrays-problem10(using brutal force approach)

--- BS_on_1D_arrays/minimum_rotated_sorted_array.py
+++ BS_on_1D_arrays/minimum_rotated_sorted_array.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(N), we check every element once.
+#Space Complexity: O(1), constant additonal space is used.
+#Linear search approach to find the minimum element in a rotated sorted array.
 def minimum(arr,n):
     min=arr[0]
     for i in range(n):
