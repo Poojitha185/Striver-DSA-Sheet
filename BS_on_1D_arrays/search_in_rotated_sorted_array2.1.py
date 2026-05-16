051ccd7096e21020257accd0ff49a71b213ba3bd
Poojitha185 <poojithap741@gmail.com>
2026-05-16 22:15:10 +0530

    Day66-solving problems on BS on 1D arrays-problem9(using optimal approach)

--- BS_on_1D_arrays/search_in_rotated_sorted_array2.1.py
+++ BS_on_1D_arrays/search_in_rotated_sorted_array2.1.py
@@ -1,43 +1,25 @@
-class Solution:
-    def search_in_rotated_sorted_array_ii(self, arr, k):
-        low, high = 0, len(arr) - 1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            # If mid is the target
-            if arr[mid] == k:
-                return True
-
-            # Cannot determine sorted half due to duplicates
-            if arr[low] == arr[mid] == arr[high]:
+def search(arr,n,target):
+    low,high=0,n-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            return True
+        if arr[low] == arr[mid] == arr[high]:
                 low += 1
                 high -= 1
                 continue
-
-            # Left half is sorted
-            if arr[low] <= arr[mid]:
-                if arr[low] <= k <= arr[mid]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
+        if arr[low]<=arr[mid]:
+            if arr[low]<=target<arr[mid]:
+                high=mid-1
             else:
-                # Right half is sorted
-                if arr[mid] <= k <= arr[high]:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-
-        return False
-
-
-if __name__ == "__main__":
-    arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6]
-    k = 3
-    sol = Solution()
-    result = sol.search_in_rotated_sorted_array_ii(arr, k)
-
-    if result:
-        print("Target is present in the array.")
-    else:
-        print("Target is not present.")
+                low=mid+1
+        else:
+            if arr[mid]<target<=arr[high]:
+                low=high+1
+            else:
+                high=mid-1
+    return False
+arr=list(map(int,input("enter the array: ").split(',')))
+n=len(arr)
+target=int(input("enter the target: "))
+print(search(arr,n,target))
