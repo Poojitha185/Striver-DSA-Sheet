6f5ada137e01eb5ad91fd1e9df7b0097759373e1
Poojitha185 <poojithap741@gmail.com>
2026-05-17 21:59:34 +0530

    Day65-solving problems on BS on 1D arrays-problem10(using optimal(binary search) approach)

--- BS_on_1D_arrays/minimum_rotated_sorted_array.1.py
+++ BS_on_1D_arrays/minimum_rotated_sorted_array.1.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(logN), at every step the search space is reduced to half using binary search.
+#Space Complexity: O(1), constant additonal space is used.
+#using binary search approach
 def minimum_rotated_sorted_array(arr,n):
     ans = float('inf')  #inf=infinity,We use this often when finding minimum element,minimum distance,smallest value and similarily for maximum element we use -inf.
     low,high=0,n-1
