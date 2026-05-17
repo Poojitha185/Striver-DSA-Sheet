3facee31bef3c921f0278f0e0edf6ef7717eae44
Poojitha185 <poojithap741@gmail.com>
2026-05-17 21:58:26 +0530

    Day65-solving problems on BS on 1D arrays-problem10(using optimal approach)

---
+++ BS_on_1D_arrays/minimum_rotated_sorted_array.1.py
@@ -0,0 +1,19 @@
+def minimum_rotated_sorted_array(arr,n):
+    ans = float('inf')  #inf=infinity,We use this often when finding minimum element,minimum distance,smallest value and similarily for maximum element we use -inf.
+    low,high=0,n-1
+    while(low<=high):
+        mid=(low+high)//2
+        if arr[low]<=arr[high]:
+            ans=min(ans,arr[low])
+            break
+        if arr[low]<=arr[mid]:
+            ans=min(ans,arr[low])
+            low=mid+1
+        else:
+            ans=min(ans,arr[mid])
+            high=mid-1
+    return ans
+arr=list(map(int,input("enter the array: ").split(',')))
+n=len(arr)
+print("The minimum element in the array is:",minimum_rotated_sorted_array(arr,n))
+
