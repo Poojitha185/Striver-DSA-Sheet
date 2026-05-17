732a194322360ceff581b1975cc88c9fb6fe52d5
Poojitha185 <poojithap741@gmail.com>
2026-05-17 22:16:41 +0530

    Day67-solving problems on BS on 1D arrays-problem11(using brute force(linear search) approach)

--- BS_on_1D_arrays/no_of_times_array_rotated.py
+++ BS_on_1D_arrays/no_of_times_array_rotated.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(n),We scan the entire array once to find the smallest element, where n is the size of the array.
+
+#Space Complexity: O(1),We only use a few extra variables to store the minimum value and its index, so the extra space used is constant.
+
+#we simply search for the smallest element in the array because it’s the point where the rotation happened its index directly tells us the rotation count
+
+#using linear search approach to find the index of the minimum element which is also the number of times the array is rotated.
+
 def no_of_rotations(arr,n):
     min=arr[0]
     for i in range(n):
