a8f2875a76a592f33a66f682ee1293f0a219d0cc
Poojitha185 <poojithap741@gmail.com>
2026-05-18 19:39:37 +0530

    Day68-solving problems on BS on 1D arrays-problem11(using optimal(binary search) approach)

--- BS_on_1D_arrays/no_of_times_array_rotated.2.py
+++ BS_on_1D_arrays/no_of_times_array_rotated.2.py
@@ -1,12 +1,18 @@
+#Time Complexity: O(log n),The binary search halves the search space each iteration, where n is the size of the array.
+
+#Space Complexity: O(1),Only a few variables are used regardless of input size, so constant extra space.
+
+#using binary search
+
 def rotations_count(arr,n):
     low,high=0,n-1
-    while low<high:
+    while low<high:     #we use low<high not low<=high because for low<=high then loop runs one extra time unnecessarily.
         mid=(low+high)//2
         if arr[mid]>arr[high]:
             low=mid+1
         else:
             high=mid
-    return low
+    return low         #when low==high then the index of low will be the no of rotations
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
 print("The number of times the array is rotated is:",rotations_count(arr,n))
