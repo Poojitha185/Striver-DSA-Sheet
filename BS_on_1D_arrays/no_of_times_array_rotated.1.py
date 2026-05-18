ee713cb082af4bdda9560127801c148892e304d5
Poojitha185 <poojithap741@gmail.com>
2026-05-18 19:00:41 +0530

    Day68-solving problems on BS on 1D arrays-problem11(using better(with same time complexity as brutal force) approach)

--- BS_on_1D_arrays/no_of_times_array_rotated.1.py
+++ BS_on_1D_arrays/no_of_times_array_rotated.1.py
@@ -1,3 +1,7 @@
+#Time Complexity: O(n),We traverse the array once to find the rotation point, where n is the size of the array.
+
+#Space Complexity: O(1),Only a few extra variables are used regardless of input size, so constant space.
+
 def rotation_count(arr,n):
     for i in range(n-1):
         if arr[i+1]>arr[i]:
