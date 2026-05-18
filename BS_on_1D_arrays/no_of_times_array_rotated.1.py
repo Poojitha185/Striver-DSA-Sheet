6b473e3cd42666621571b89f3098871c45b2f84b
Poojitha185 <poojithap741@gmail.com>
2026-05-18 19:28:19 +0530

    Day68-solving problems on BS on 1D arrays-problem11(using better(with same time complexity as brutal force) approach)

--- BS_on_1D_arrays/no_of_times_array_rotated.1.py
+++ BS_on_1D_arrays/no_of_times_array_rotated.1.py
@@ -4,9 +4,10 @@
 
 def rotation_count(arr,n):
     for i in range(n-1):
-        if arr[i+1]>arr[i]:
+        if arr[i+1]<arr[i]:
             return i+1
     return 0
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
 print("The number of times the array is rotated is:",rotation_count(arr,n))
+
