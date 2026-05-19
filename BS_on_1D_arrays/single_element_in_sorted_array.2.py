05fd36c9382c464f6f032caea57be54981d60cab
Poojitha185 <poojithap741@gmail.com>
2026-05-19 21:56:21 +0530

    Day69-solving problems on BS on 1D arrays-problem12(using brute force2(using xor operation)approach)

--- BS_on_1D_arrays/single_element_in_sorted_array.2.py
+++ BS_on_1D_arrays/single_element_in_sorted_array.2.py
@@ -1,3 +1,9 @@
+#Time Complexity: O(N), N = size of the given array.We are traversing the entire array.
+
+#Space Complexity: O(1) as we are not using any extra space.
+
+#using XOR operation, we can find the single element in the array. The XOR of a number with itself is 0, and the XOR of a number with 0 is the number itself. So, if we XOR all the elements in the array, the duplicate elements will cancel out each other and we will be left with the single element.
+
 def single_element(arr,n):
     ans=0
     for i in range(n):
