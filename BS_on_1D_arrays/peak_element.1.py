e1b8c0d7a729c1959205772397fb848f0f5100cd
Poojitha185 <poojithap741@gmail.com>
2026-05-20 22:44:07 +0530

    Day70-solving problems on BS on 1D arrays-problem13(using optimal(binary search) approach)

--- BS_on_1D_arrays/peak_element.1.py
+++ BS_on_1D_arrays/peak_element.1.py
@@ -1,3 +1,8 @@
+#Time Complexity: O(log N), we reduce the search space to half at every step using binary search.
+#Space Complexity: O(1), constant additional space is used.
+#we know that if current element is greater than its left neighbour, we are in the left half and if our current element is greater than its right neighbour then we are in the right half. If we know the half that we are in currently, we can eliminate it to find our peak element.
+#using binary search
+
 def peak_element(arr,n):
     low,high=0,n-1
     while low<high:
