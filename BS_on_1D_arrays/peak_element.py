130ee408faa6288c8b4e12a7014e9ed7b286c5a7
Poojitha185 <poojithap741@gmail.com>
2026-05-20 20:07:38 +0530

    Day70-solving problems on BS on 1D arrays-problem13(using Brute force approach)

--- BS_on_1D_arrays/peak_element.py
+++ BS_on_1D_arrays/peak_element.py
@@ -1,3 +1,7 @@
+#Time Complexity: O(N), we traverse the entire array once to find peak element.
+#Space Complexity: O(1), constant additional space is used.
+#A peak element is an element that is greater than or equal to its neighbors. To find a peak element in an array, we can iterate through the array and check if the current element is greater than or equal to its left and right neighbors. If it is, then we have found a peak element and we can return its index. If we reach the end of the array without finding a peak element, we can return -1.
+
 def peak_element(arr,n):
     for i in range(n):
      left=(i==0)or(arr[i]>=arr[i-1])
