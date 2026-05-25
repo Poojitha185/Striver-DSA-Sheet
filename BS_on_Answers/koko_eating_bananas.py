04ff2791b29ae5e045892ed783b2ccfcb78ec384
Poojitha185 <poojithap741@gmail.com>
2026-05-25 21:49:06 +0530

    Day71-solving problems on BS on answers-problem1(using brutal force approach)

--- BS_on_Answers/koko_eating_bananas.py
+++ BS_on_Answers/koko_eating_bananas.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(n * max(a[])), since for each possible speed we go through all the piles.
+#Space Complexity: O(1), since the algorithm does not use any additional space or data structures.
+
 import math
 from math import ceil
 def min_integer(arr,n,h):
@@ -9,7 +12,7 @@ def min_integer(arr,n,h):
 def totaltime(arr,i):
     totaltime=0
     for j in arr:
-      totaltime+=ceil(j/i)
+      totaltime+=ceil(j/i)       #ceil() means ceiling function.It rounds a number up to the nearest integer
     return totaltime
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
