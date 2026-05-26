664fcb1dab5419a944889ad6f46ba3abff4c15f7
Poojitha185 <poojithap741@gmail.com>
2026-05-26 14:21:47 +0530

    Day75-solving problems on BS on answers-problem1(using optimal approach)

--- BS_on_Answers/koko_eating_bananas.1.py
+++ BS_on_Answers/koko_eating_bananas.1.py
@@ -1,39 +1,22 @@
 import math
+from math import ceil
 
-class Solution:
-    # Function to calculate total hours at given speed
-    def calculateTotalHours(self, piles, speed):
-        totalH = 0
-        for bananas in piles:
-            totalH += math.ceil(bananas / speed)
-        return totalH
-
-    # Function to find minimum eating speed
-    def minEatingSpeed(self, piles, h):
-        # Find maximum element
-        maxPile = max(piles)
-
-        # Initialize low and high pointers
-        low, high = 1, maxPile
-        ans = maxPile
-
-        # Binary search on answer space
-        while low <= high:
-            mid = (low + high) // 2
-            totalH = self.calculateTotalHours(piles, mid)
-
-            # If possible, try smaller speed
-            if totalH <= h:
-                ans = mid
-                high = mid - 1
-            # Otherwise, try larger speed
-            else:
-                low = mid + 1
-
-        return ans
-
-# Driver code
-piles = [3, 6, 7, 11]
-h = 8
-obj = Solution()
-print(obj.minEatingSpeed(piles, h))
+def minEatingSpeed(arr,n,h):
+   low,high=1,max(arr)
+   while low<=high:
+      mid=(low+high)//2
+      reqtime=totaltime(arr,mid)
+      if reqtime<=h:
+         high=mid-1
+      else:
+         low=mid+1
+   return low
+def totaltime(arr,i):
+    totaltime=0
+    for j in arr:
+      totaltime+=ceil(j/i)       #ceil() means ceiling function.It rounds a number up to the nearest integer
+    return totaltime
+arr=list(map(int,input("enter the array: ").split(',')))
+n=len(arr)
+h=int(input("enter the hours: "))
+print("The minimum integer is:",minEatingSpeed(arr,n,h))
