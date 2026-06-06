40162079225aa188b246108e1b7a8da18068c707
Poojitha185 <poojithap741@gmail.com>
2026-06-06 18:10:37 +0530

    Day78-solving problems on BS on answers-problem5(using optimal(binary search) approach)

--- BS_on_Answers/kth_missing_positive_no.1.py
+++ BS_on_Answers/kth_missing_positive_no.1.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(logn), used for typical binary search
+
+#Space Complexity: O(1), no extra space used
+
+#We cannot apply binary search on the answer space here as we cannot assure which missing number has the possibility of being the kth missing number. That is why, we will do something different here. We will try to find the closest neighbors (i.e. Present in the array) for the kth missing number by counting the number of missing numbers for each element in the given array.
+
+#using binary search
+
 def kth_missing_number(arr,n,k):
     low,high=0,n-1
     while low<=high:
