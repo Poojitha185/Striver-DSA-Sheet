d5fe3e55917b5bec2b3877ac5e3b23614617e07d
Poojitha185 <poojithap741@gmail.com>
2026-06-06 18:07:18 +0530

    Day78-solving problems on BS on answers-problem5(using optimal approach)

--- BS_on_Answers/kth_missing_positive_no.1.py
+++ BS_on_Answers/kth_missing_positive_no.1.py
@@ -1,31 +1,14 @@
-#Time Complexity: O(logn), used for typical binary search
-
-#Space Complexity: O(1), no extra space used
-
-class MissingKFinder:
-    # Binary search to find the k-th missing number
-    def missing_k(self, vec, k):
-        low, high = 0, len(vec) - 1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            # Number of missing numbers before index mid
-            missing = vec[mid] - (mid + 1)
-
-            if missing < k:
-                low = mid + 1  # Need more missing values, go right
-            else:
-                high = mid - 1  # Too many missing, go left
-
-        # Final k-th missing number calculation
-        return k + high + 1
-
-# Driver code
-vec = [4, 7, 9, 10]
-k = 4
-
-finder = MissingKFinder()
-ans = finder.missing_k(vec, k)
-
-print("The missing number is:", ans)
+def kth_missing_number(arr,n,k):
+    low,high=0,n-1
+    while low<=high:
+        mid=(low+high)//2
+        missing=arr[mid]-(mid+1)
+        if missing<k:
+            low=mid+1
+        else:
+            high=mid-1
+    return (high+1+k)            # or return(low+k),because low=high+1 after we done with the binary serach in a arr where low ended after high
+arr = list(map(int, input("Enter the array: ").split(',')))
+k = int(input("Enter the value of k: "))
+n=len(arr)
+print("The kth missing positive number is:", kth_missing_number(arr,n,k))
