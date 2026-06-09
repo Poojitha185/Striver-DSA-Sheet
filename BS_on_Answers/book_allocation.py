e627c06af665bb3a5e3fdf25aa3976107bf9dfd5
Poojitha185 <poojithap741@gmail.com>
2026-06-09 20:38:51 +0530

    Day85-solving problems on BS on answers-problem6(using brute force(linear search) approach)

--- BS_on_Answers/book_allocation.py
+++ BS_on_Answers/book_allocation.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[])
+
+#Space Complexity:  O(1) as we are not using any extra space to solve this problem.
+
+#Next, inside the loop, we will send each ‘pages’, to the function countStudents() function to get the number of students to whom we can allocate the books.
+#The first number of pages, ‘pages’, for which the number of students will be equal to ‘m’, will be our answer. So, we will return that particular ‘pages’.
+
+
 def ispossible(arr,j,n,s):
     no_of_students=1
     pages=0
@@ -6,7 +14,7 @@ def ispossible(arr,j,n,s):
             return False
         if (pages+arr[i]>j):
             no_of_students+=1
-            pages+=arr[i]
+            pages=arr[i]                 #assign the new value of arr to pages when we start to assign pages for new students
         else:
             pages+=arr[i]
     if (no_of_students>s):
@@ -15,12 +23,11 @@ def ispossible(arr,j,n,s):
 def allocation(arr,n,s):
     if s>n:
         return -1
-    low=min(arr)
-    high=sum(arr)
+    low,high=max(arr),sum(arr)                     #Minimum possible answer = max(arr) (largest book) Maximum possible answer = sum(arr) (one student gets all books)
     for j in range(low,high+1):
         if(ispossible(arr,j,n,s)==True):
             return j
-    return -1
+    return low
 arr=list(map(int,input("enter the array: ").split(',')))
 n=len(arr)
 s=int(input("enter the number of students: "))
