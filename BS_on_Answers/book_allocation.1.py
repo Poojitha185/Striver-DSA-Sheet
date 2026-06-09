3c36e071f39893ddb0b3113b5673cf4f39b9fac4
Poojitha185 <poojithap741@gmail.com>
2026-06-09 20:53:53 +0530

    Day86-solving problems on BS on answers-problem6(using optimal(binary search) approach)

--- BS_on_Answers/book_allocation.1.py
+++ BS_on_Answers/book_allocation.1.py
@@ -1,3 +1,13 @@
+#Time Complexity: O(N * log(sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.
+
+#Space Complexity:  O(1) as we are not using any extra space to solve this problem.
+
+#We perform Binary Search:
+#We try a middle value of pages per student.
+#We check how many students would be required if no student gets more than that value.
+#If it takes more students than allowed, that value is too low, so we try a higher one.
+#If it fits within the allowed number of students, we store it and try a smaller one to find an even better option.
+
 def ispossible(arr,j,n,s):
     no_of_students=1
     pages=0
@@ -6,7 +16,7 @@ def ispossible(arr,j,n,s):
             return False
         if (pages+arr[i]>j):
             no_of_students+=1
-            pages=arr[i]                 #assign the new value of arr to pages when we start to assign pages for new students
+            pages=arr[i]     #assign the new value of arr to pages when we start to assign pages for new students
         else:
             pages+=arr[i]
     if (no_of_students>s):
@@ -16,9 +26,9 @@ def allocation(arr,n,s):
     if s>n:
         return -1
     low=max(arr)
-    high=sum(arr)
+    high=sum(arr)             #The minimum possible is the largest single book (because every student must get at least one complete book),The maximum possible is the sum of all pages (if one student reads all books).
     while(low<=high):
-        mid=(low+high)//2
+        mid=(low+high)//2         
         if(ispossible(arr,mid,n,s)==True):
             high=mid-1
         else:
