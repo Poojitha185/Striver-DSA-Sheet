1f313b7c9695b806b57e34b7d363c70f7e7fbd91
Poojitha185 <poojithap741@gmail.com>
2026-06-10 20:21:28 +0530

    Day87-solving problems on BS on answers-problem7(using optimal(binary search) approach)

--- BS_on_Answers/median_of_two_sorted_arrays.1.py
+++ BS_on_Answers/median_of_two_sorted_arrays.1.py
@@ -1,3 +1,7 @@
+# TC: O(log(min(m,n)))
+# SC: O(1) because no extra data structure is used,
+# Always perform binary search on the smaller array
+# to achieve O(log(min(m,n))) complexity.
 def median(arr1, arr2):
     m, n = len(arr1), len(arr2)
 
@@ -9,11 +13,11 @@ def median(arr1, arr2):
     high = m
 
     while low <= high:
-        cut1 = (low + high) // 2
-        cut2 = (m + n + 1) // 2 - cut1
+        cut1 = (low + high) // 2                              #cut1 = number of elements taken from arr1 into the left half.
+        cut2 = (m + n + 1) // 2 - cut1                        #cut2 = remaining elements required from arr2 ,so that the left half contains exactly (m+n+1)//2 elements. 
 
-        l1 = float('-inf') if cut1 == 0 else arr1[cut1 - 1]
-        l2 = float('-inf') if cut2 == 0 else arr2[cut2 - 1]
+        l1 = float('-inf') if cut1 == 0 else arr1[cut1 - 1]   #If cut1 is 0, it means we are not taking any elements from arr1 into the left half, so we assign l1 to negative infinity to ensure it does not affect the maximum calculation. Otherwise, l1 is the last element of the left half from arr1.
+        l2 = float('-inf') if cut2 == 0 else arr2[cut2 - 1]   #If cut2 is 0, it means we are not taking any elements from arr2 into the left half, so we assign l2 to negative infinity to ensure it does not affect the maximum calculation. Otherwise, l2 is the last element of the left half from arr2.
 
         r1 = float('inf') if cut1 == m else arr1[cut1]
         r2 = float('inf') if cut2 == n else arr2[cut2]
@@ -25,7 +29,7 @@ def median(arr1, arr2):
 
             return max(l1, l2)
 
-        elif l1 > r2:
+        elif l1 > r2:                     # If l1 > r2, we have taken too many elements from arr1, so move the cut to the left.
             high = cut1 - 1
 
         else:
