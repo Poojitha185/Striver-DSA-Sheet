6e89f5416c19191c5cd89a20f0f5ad3dd889725e
Poojitha185 <poojithap741@gmail.com>
2026-06-12 20:43:37 +0530

    Day89-solving problems on BS on 2D arrays-problem1(using brute force(linear search) approach)

--- BS_on_2D_arrays/search_in_2D_matrix.py
+++ BS_on_2D_arrays/search_in_2D_matrix.py
@@ -1,3 +1,9 @@
+#Time Complexity: O(n × m), We are traversing the entire matrix with `n` rows and `m` columns. In the worst case, we may end up visiting every cell once if the target is not present. So, the total number of operations is proportional to the number of elements in the matrix.
+
+#Space Complexity: O(1),We are not using any additional space. The algorithm uses a constant amount of extra memory regardless of the size of the matrix just loop variables and the target. Therefore, the space complexity is constant.
+
+#using linear search
+
 def search(arr,n,m,target):
     for i in range(n):
         for j in range(m):
