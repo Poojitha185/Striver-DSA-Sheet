0e2c96c774b23e347283d656139354b0fd5b0592
Poojitha185 <poojithap741@gmail.com>
2026-06-13 19:31:28 +0530

    Day90-solving problems on BS on 2D arrays-problem1(using optimal approach(binary search) approach)

--- BS_on_2D_arrays/search_in_2D_matrix.2.py
+++ BS_on_2D_arrays/search_in_2D_matrix.2.py
@@ -1,3 +1,12 @@
+#Time Complexity: O(log(NxM)), where N = given row number, M = given column number.We are applying binary search on the imaginary 1D array of size NxM.
+
+#Space Complexity: O(1) as we are not using any extra space.
+
+#If we flatten the given 2D matrix into a 1D array, that 1D array would also be sorted. By running binary search on this flattened version, we could quickly check if the element exists.
+#But actually flattening the matrix takes extra time and memory, which makes it inefficient. Instead, we can simulate the flattening without creating a new array. The trick is to directly map a 1D index into the corresponding row and column of the 2D matrix.
+#To do this mapping, if there are `m` columns in the matrix and the index is `i`, then:
+#Row = i // m, Column = i % m.
+
 def binary_search(matrix,n,m,target):
     low,high=0,(n*m-1)
     while low<=high:
