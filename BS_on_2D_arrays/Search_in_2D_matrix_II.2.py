0dbb799f4d01506022940f968520d041ebef6053
Poojitha185 <poojithap741@gmail.com>
2026-06-15 21:38:28 +0530

    Day92-solving problems on BS on 2D arrays-problem2(using optimal approach)

--- BS_on_2D_arrays/Search_in_2D_matrix_II.2.py
+++ BS_on_2D_arrays/Search_in_2D_matrix_II.2.py
@@ -1,3 +1,12 @@
+#Time Complexity: O(N+M), where N = given row number, M = given column number. We are starting traversal from (0, M-1), and at most, we can end up being in the cell (M-1, 0). So, the total distance can be at most (N+M). So, the time complexity is O(N+M).
+
+#Space Complexity: O(1) as we are not using any extra space.
+
+#We will do the following steps until row < n and col >= 0(i.e. while(row < n && col >= 0)):
+#If matrix[row][col] == target: We have found the target and so we will return true.
+#If matrix[row][col] > target: We need the smaller elements to reach the target. But the column is in increasing order and so it contains only greater elements. So, we will eliminate the column by decreasing the current column value by 1(i.e. col--) and thus we will move row-wise.
+#If matrix[row][col] < target: In this case, We need the bigger elements to reach the target. But the row is in decreasing order and so it contains only smaller elements. So, we will eliminate the row by increasing the current row value by 1(i.e. row++) and thus we will move column-wise.
+
 def search(matrix,target,n,m):
     row=0
     col=m-1
