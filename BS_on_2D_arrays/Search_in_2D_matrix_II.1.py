66ff160075cb08ba1cb8a7c0c165c4d02ede7b5a
Poojitha185 <poojithap741@gmail.com>
2026-06-15 21:42:37 +0530

    Day91-solving problems on BS on 2D arrays-problem2(using better(binary search for each row) approach)

--- BS_on_2D_arrays/Search_in_2D_matrix_II.1.py
+++ BS_on_2D_arrays/Search_in_2D_matrix_II.1.py
@@ -11,18 +11,18 @@ def binary_search(row,m,target):
     while low<=high:
         mid=(low+high)//2
         if row[mid]==target:
-            return mid
+            return True
         elif row[mid]<target:
             low=mid+1
         else:
             high=mid-1
-    return -1
+    return False
 def search(matrix,target,n,m):
     for i in range(n):
         ind=binary_search(matrix[i],m,target)
-        if ind!=-1:
-            return (i,ind)
-    return(-1,-1)
+        if ind!=False:
+            return True
+    return False
 rows = int(input("Enter number of rows: "))
 matrix = []
 for i in range(rows):
@@ -30,5 +30,5 @@ for i in range(rows):
     matrix.append(row)
 cols = len(matrix[0]) if matrix else 0
 target = int(input("Enter the target element: "))
-print("The target element is found at: ",search(matrix,target,rows,cols))
+print("Is the target element present in the 2D array?",search(matrix,target,rows,cols))
 
