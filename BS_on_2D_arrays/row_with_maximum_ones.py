5e220e56178246abeb4a4cc5524aab8d3c7ea769
Poojitha185 <poojithap741@gmail.com>
2026-06-16 21:49:05 +0530

    Day93-solving problems on BS on 2D arrays-problem3(using brute force approach)

--- BS_on_2D_arrays/row_with_maximum_ones.py
+++ BS_on_2D_arrays/row_with_maximum_ones.py
@@ -1,3 +1,10 @@
+#Time Complexity:O(n X m), where n = given row number, m = given column number. We are using nested loops running for n and m times respectively.
+
+#Space Complexity: O(1). No extra space used
+#First, we create two variables: cnt_max is set to 0 and will keep track of the highest number of 1s found so far, and index is set to -1 and will store the row number with the most 1s.
+#Then, we go through each row of the matrix one by one using a loop.
+#Inside that loop, for each row, we count how many 1s it contains using another loop.
+
 def row_maximum_ones(matrix,n,m):
     cnt_max=0
     index=-1
