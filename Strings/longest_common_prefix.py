fe4ea8675e9c74851398cf4500ecea175bbf0ba6
Poojitha185 <poojithap741@gmail.com>
2026-06-19 21:24:19 +0530

    Day95-solving Basic and Easy String problems-problem2

--- Strings/longest_common_prefix.py
+++ Strings/longest_common_prefix.py
@@ -1,41 +1,12 @@
-#Time Complexity: O(N * log N + M), where N is the number of strings and M is the minimum length of a string. The sorting operation takes O(N * log N) time, and the comparison of characters in the first and last strings takes O(M) time.
-
-#Space Complexity: O(M), as the ans variable can store the length of the prefix which in the worst case will be O(M).
-
-#The common prefix across all strings must exist between the smallest and largest string when sorted lexicographically.
-#Sorting the array helps bring these boundary strings to the extremes.
-class Solution:
-    def longestCommonPrefix(self, strs):
-        if not strs:
-            return ""
-        strs.sort()
-        
-        # First string in sorted list
-        first = strs[0]
-        
-        # Last string in sorted list
-        last = strs[-1]
-        ans = []
-        for i in range(min(len(first), len(last))):
-            # Stop if characters differ
-            if first[i] != last[i]:
-                return ''.join(ans)
-            # Add matching character to result
-            ans.append(first[i])
-        
-        # Return the longest common prefix
-        return ''.join(ans)
-
-# Run the function with sample input
-if __name__ == "__main__":
-    # Create an instance of Solution
-    solution = Solution()
-    
-    # Input list of strings
-    input_strs = ["interview", "internet", "internal", "interval"]
-    
-    # Call the method to find prefix
-    result = solution.longestCommonPrefix(input_strs)
-    
-    # Print the result
-    print("Longest Common Prefix:", result)  
+def largest_common_prefix(strs,n):
+    first=strs[0]
+    last=strs[-1]
+    ans=[]
+    for i in range(min(len(first),len(last))):
+        if first[i]!=last[i]:
+            return ''.join(ans)
+        ans.append(first[i])
+    return ''.join(ans)
+strs = eval(input("Enter list of strings: "))
+n=len(strs)
+print("The longest common prefix is:", largest_common_prefix(strs,n))
