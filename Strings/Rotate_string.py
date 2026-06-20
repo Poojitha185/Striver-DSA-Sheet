9280877467444c3ae4cd6122192825d3cb5ae233
Poojitha185 <poojithap741@gmail.com>
2026-06-20 22:30:35 +0530

    Day97-solving Basic and Easy String problems-problem4(using brute force approach)

--- Strings/Rotate_string.py
+++ Strings/Rotate_string.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(N^2) since generating N rotations and each comparison takes O(N) time.
+
+#Space Complexity: O(N) for the space needed to store each rotated string.
+
+#Start by generating all possible left rotations of the original string using substring slicing and concatenation.
+
+#For each rotated version of the string, compare it with the target (goal) string.
+
 def rotate_string(s,goal):
     if len(s)!=len(goal):
         return False
