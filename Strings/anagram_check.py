67519874bd1d7bbdb73e3ba49b4fbb234e98c08c
Poojitha185 <poojithap741@gmail.com>
2026-06-21 21:37:39 +0530

    Day98-solving Basic and Easy String problems-problem6(using brute force aproach)

---
+++ Strings/anagram_check.py
@@ -0,0 +1,8 @@
+def anagram_check(s1,s2):
+    for i in range(len(s1)):
+        if s1[i] not in s2:
+          return False
+    return True
+s1=input("enter thr first string: ")
+s2=input("enter the second string: ")
+print("does both the strings anagram of each other: ",anagram_check(s1,s2))
