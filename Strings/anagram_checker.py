3ae3a7a446fb7f3df62d3b5e629e2a6c181245a5
Poojitha185 <poojithap741@gmail.com>
2026-06-21 21:51:25 +0530

    Day98-solving Basic and Easy String problems-problem5

--- Strings/anagram_checker.py
+++ Strings/anagram_checker.py
@@ -1,6 +1,6 @@
 #TC:O(N^2)
 #SC:O(1)
-def anagram_check(s1,s2):
+def anagram_checker(s1,s2):
     if len(s1)!=len(s2):
         return False
     for i in range(len(s1)):
@@ -9,4 +9,4 @@ def anagram_check(s1,s2):
     return True
 s1=input("enter thr first string: ")
 s2=input("enter the second string: ")
-print("does both the strings anagram of each other: ",anagram_check(s1,s2))
+print("does both the strings anagram of each other: ",anagram_checker(s1,s2))
