feebdebc3e79f342cb988fb5b2d7e863ad532a13
Poojitha185 <poojithap741@gmail.com>
2026-06-22 22:26:43 +0530

    Day99-solving Basic and Easy String problems-problem6

--- Strings/reverse_words_in_string.py
+++ Strings/reverse_words_in_string.py
@@ -1,7 +1,9 @@
+#TC:O(N) where N is the number of characters in the input string, due to the single loop iterating through the words.
+#SC:O(N) for the space used to store the list of words and the reversed list.
 def reverse(s):
     t=s.split(" ")
     k=[]
-    for i in range(len(t)-1,-1,-1):
+    for i in range(len(t)-1,-1,-1):                 #or using builtin function t.reverse()  and return " ".join(t)
         k.append(t[i])
     return " ".join(k)
 s=input("enter string:")
