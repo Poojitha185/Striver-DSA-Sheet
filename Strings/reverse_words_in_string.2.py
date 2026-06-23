9abe595790ac6bafb4f798a7bf83af2b11c61c99
Poojitha185 <poojithap741@gmail.com>
2026-06-23 20:58:54 +0530

    Day100-solving Basic and Easy String problems-problem6(using optimal approach)

--- Strings/reverse_words_in_string.2.py
+++ Strings/reverse_words_in_string.2.py
@@ -1,3 +1,13 @@
+#Time Complexity: O(N), We traverse the string once from right to left and construct the result directly without extra passes.
+
+#Space Complexity: O(1),Ignoring the output string, no additional data structures proportional to input size are used.
+
+#Initialize an empty result string.Set a pointer at the last character of the string. While the pointer is within the string:. Skip all spaces to move to the end of a word. Mark the end position of the word.
+
+#Move the pointer backward until a space or start of string is found.
+
+#Extract the word and append it to the result string.
+
 def reverse(s):
     result=""
     i=len(s)-1
