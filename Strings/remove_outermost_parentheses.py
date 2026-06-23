cd0447256db0ec4a4a10628d1ad820f6c07f0f0e
Poojitha185 <poojithap741@gmail.com>
2026-06-23 22:04:56 +0530

    Day100-solving Basic and Easy String problems-problem7(using level counter)

--- Strings/remove_outermost_parentheses.py
+++ Strings/remove_outermost_parentheses.py
@@ -1,3 +1,11 @@
+#Time Complexity: O(n), since we are performing a single traversal of the string.
+
+#Space Complexity: O(1), since we are using a few variables to track the current state.
+
+#If the current character is '(', increment the level counter. If the level is greater than 1 (indicating we're inside a valid primitive), add '(' to the result string
+
+#If the current character is ')', decrement the level counter. If the level is greater than 0 (indicating we're still inside a valid primitive), add ')' to the result string
+
 def remove_outermost_parentheses(s):
     result=""
     level=0
