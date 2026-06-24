8ad020794a5c437e1bde62dfb96c9a3f1016bdc1
Poojitha185 <poojithap741@gmail.com>
2026-06-24 21:20:42 +0530

    Day101-solving medium String problems-problem8(using dictionary method)

--- Strings/sort_chars_frequency.py
+++ Strings/sort_chars_frequency.py
@@ -10,7 +10,8 @@ def sort_chars_by_frequency(s):
     result = sorted(k.items(), key=lambda x: (-x[1], x[0]))  #-x[1] Makes the frequency sort in descending order.
                                                              # x[0] If two characters have the same frequency, sort them alphabetically
     for key,item in result:
-        l.append(key)
+        if item>0:
+         l.append(key)
     return l
 s=input("enter the string: ")
 print("the sorted string based on frequency of characters is: ",sort_chars_by_frequency(s))
