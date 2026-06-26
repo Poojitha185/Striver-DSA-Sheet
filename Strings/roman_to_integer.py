c1f372d86949d4ecbd6fa5a0057d1f6554222b99
Poojitha185 <poojithap741@gmail.com>
2026-06-26 21:49:22 +0530

    Day103-solving medium String problems-problem10(using loop)

--- Strings/roman_to_integer.py
+++ Strings/roman_to_integer.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(n), where n is the length of the input string since we traverse the string once.
+#Space Complexity: O(1), since we use a fixed-size map for Roman numerals.
+
 def roman_to_integer(s):
     roman = {
             'I': 1, 'V': 5, 'X': 10,
@@ -9,7 +12,8 @@ def roman_to_integer(s):
             ans-=roman[s[i]]
         else:
             ans+=roman[s[i]]
-    ans+=roman[s[-1]]
+    ans+=roman[s[-1]]                  #The final character is always added since there's nothing after it to compare.
+
     return ans
 s=input("Enter the string: ")
 print(roman_to_integer(s))
