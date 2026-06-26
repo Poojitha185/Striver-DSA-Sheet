f5a22dafe51edc6dca490dee29df8997f3bff307
Poojitha185 <poojithap741@gmail.com>
2026-06-26 21:53:18 +0530

    Day103-solving medium String problems-problem10

--- Strings/roman_to_integer.py
+++ Strings/roman_to_integer.py
@@ -1,6 +1,6 @@
 #Time Complexity: O(n), where n is the length of the input string since we traverse the string once.
 #Space Complexity: O(1), since we use a fixed-size map for Roman numerals.
-#However, when a smaller value appears before a larger one, it indicates subtraction instead of addition.To handle this, scan the string from left to right and compare each character with the one after it.
+#when a smaller value appears before a larger one, it indicates subtraction instead of addition.To handle this, scan the string from left to right and compare each character with the one after it.
 #If the current symbol is smaller than the next, treat it as a subtractive pair.Otherwise, treat the symbol as a standalone value and add it normally.
 def roman_to_integer(s):
     roman = {
