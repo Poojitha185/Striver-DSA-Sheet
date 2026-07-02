edf5fa112542fbc436012e6358ac00d83b6b61a2
Poojitha185 <poojithap741@gmail.com>
2026-07-02 22:53:50 +0530

    Day105-solving medium String problems-problem13

--- Strings/count_no_of_substrings.py
+++ Strings/count_no_of_substrings.py
@@ -6,18 +6,15 @@
 def at_most_k_distinct(s, k):
     left, res = 0, 0
     freq = {}
-
     # Iterate with right pointer
     for right in range(len(s)):
         freq[s[right]] = freq.get(s[right], 0) + 1
-
         # Shrink window if distinct characters exceed k
         while len(freq) > k:
             freq[s[left]] -= 1
             if freq[s[left]] == 0:
                 del freq[s[left]]
             left += 1
-
         # Count substrings in current window
         res += (right - left + 1)
     return res
