b85f6e705b2e4583992835c79811ab3c30e70a68
Poojitha185 <poojithap741@gmail.com>
2026-07-02 22:46:44 +0530

    Day105-solving medium String problems-problem13

--- Strings/count_no_of_substrings.py
+++ Strings/count_no_of_substrings.py
@@ -1,4 +1,8 @@
 # Function to count substrings with at most k distinct characters
+#Time Complexity: O(n) for each call to atMostKDistinct.
+
+#Space Complexity: O(1) map size bounded by 26 characters for alphabets.
+
 def at_most_k_distinct(s, k):
     left, res = 0, 0
     freq = {}
@@ -21,3 +25,7 @@ def at_most_k_distinct(s, k):
 # Function to count substrings with exactly k distinct characters
 def count_substrings(s, k):
     return at_most_k_distinct(s, k) - at_most_k_distinct(s, k - 1)
+s=input("Enter the string: ")
+k=int(input("Enter the value of k: "))
+result = count_substrings(s, k)
+print("Number of substrings with exactly", k, "distinct characters:", result)
