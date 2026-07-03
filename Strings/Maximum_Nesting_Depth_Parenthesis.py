492b4c5247bb1b8a024232e6dd80f25d887725ed
Poojitha185 <poojithap741@gmail.com>
2026-07-03 22:54:32 +0530

    Day106-solving medium String problems-problem12

--- Strings/Maximum_Nesting_Depth_Parenthesis.py
+++ Strings/Maximum_Nesting_Depth_Parenthesis.py
@@ -1,11 +1,9 @@
 #Time Complexity: O(n), where n is the length of the string.
 
-#Space Complexity: O(1), as only constant extra space is used.
- 
+#Space Complexity: O(1), as only constant extra space is used. 
 # We're interested in measuring how deeply nested the parentheses are at any point in the string. Each opening bracket indicates going deeper into a new level of nesting.
 #Each closing bracket signals returning to the previous level of nesting.
 #By tracking how deep we go at each step, we can monitor the peak nesting level
-
 
 def maxDepth(str):
         p = 0  
