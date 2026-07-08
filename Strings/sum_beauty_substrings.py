993e2a0bb177be35fd105a10c3a4583a6b05164b
Poojitha185 <poojithap741@gmail.com>
2026-07-08 20:00:00 +0530

    Backdated commit

--- Strings/sum_beauty_substrings.py
+++ Strings/sum_beauty_substrings.py
@@ -11,25 +11,16 @@
 def beauty_sum(s):
     n = len(s)
     total = 0
-
-    # Loop over all substrings
     for i in range(n):
         freq = {}
 
         for j in range(i, n):
-            # Increment frequency
             freq[s[j]] = freq.get(s[j], 0) + 1
-
             values = freq.values()
             maxi = max(values)
             mini = min(values)
-
-            # Add difference
             total += (maxi - mini)
-
     return total
-
-# Main function
 def main():
     s = "xyx"
     print("Beauty Sum:", beauty_sum(s))
