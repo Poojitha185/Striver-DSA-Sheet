ac0ecd2b3d48e07691b5179ceee913297e267f38
Poojitha185 <poojithap741@gmail.com>
2026-07-13 20:00:00 +0530

    Backdated commit

--- Strings/square_root_number.py
+++ Strings/square_root_number.py
@@ -1,3 +1,6 @@
+#Time Complexity: O(N), we check for every number from 1 to N.
+#Space Complexity: O(1), since the algorithm does not use any additional space or data structures.
+
 class Solution:
     # Function to find floor of square root using linear search
     def floorSqrt(self, n: int) -> int:
