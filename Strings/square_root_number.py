0dc113a5c9316a33e2bb45604567c05016277d8a
Poojitha185 <poojithap741@gmail.com>
2026-07-14 20:00:00 +0530

    Backdated commit

--- Strings/square_root_number.py
+++ Strings/square_root_number.py
@@ -1,6 +1,9 @@
 #Time Complexity: O(N), we check for every number from 1 to N.
 #Space Complexity: O(1), since the algorithm does not use any additional space or data structures.
-
+#Start by creating a variable called ans to hold the result and run a loop from 1 up to n.
+#While the square of the current number is less than or equal to n, keep updating ans with that number.
+#As soon as the square of the number becomes greater than n, stop the loop because no bigger number can be the answer.
+#At the end, the value stored in ans will be the integer square root of n.
 class Solution:
     # Function to find floor of square root using linear search
     def floorSqrt(self, n: int) -> int:
