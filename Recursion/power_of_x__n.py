a5fa05397a021386df2cb87fd1764afbb034b5c8
Poojitha185 <poojithap741@gmail.com>
2026-07-18 23:10:59 +0530

    Day109-solving Recursion get strong hold problem2

--- Recursion/power_of_x__n.py
+++ Recursion/power_of_x__n.py
@@ -1,5 +1,17 @@
-def power(x,n):
-    return float(x)**n
-x=float(input("enter x value: "))
-n=int(input("enter power value: "))
-print(power(x,n))
+def myPow(x, n):
+        if n == 0 or x == 1.0:
+            return 1
+        temp = n  # to avoid integer overflow
+        # Handle negative exponents
+        if n < 0:
+            x = 1 / x
+            temp = -1 * n
+        ans = 1
+        for i in range(temp):
+            # Multiply ans by x for n times
+            ans *= x 
+        return float(ans)
+x=float(input("Enter the value of x: "))
+n=int(input("Enter the value of n: "))
+print(myPow(x, n))
+
