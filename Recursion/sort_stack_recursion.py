d3eb92e02a5b01fc6dd60da66b755ca3d0c09080
Poojitha185 <poojithap741@gmail.com>
2026-07-20 20:00:00 +0530

    Backdated commit

---
+++ Recursion/sort_stack_recursion.py
@@ -0,0 +1,16 @@
+def insert(stack,temp):
+    if not stack or stack[-1]<=temp:
+        stack.append(temp)
+        return
+    val=stack.pop()
+    insert(stack,temp)
+    stack.append(val)
+def sortstack(stack):
+    if stack:
+        temp=stack.pop()
+        sortstack(stack)
+        insert(stack,temp)
+stack=list(map(int,input("enter the array: ").split(',')))
+print("The sorted stack is:",end=" ")
+sortstack(stack)
+print(stack)
