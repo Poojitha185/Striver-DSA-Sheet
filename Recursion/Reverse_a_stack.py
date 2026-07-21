da649445db4de1da539e3a635ddf1306c5ffe102
Poojitha185 <poojithap741@gmail.com>
2026-07-21 23:39:30 +0530

    Day111-solving Recursion get strong hold problem4(using recursion)

--- Recursion/Reverse_a_stack.py
+++ Recursion/Reverse_a_stack.py
@@ -1,11 +1,11 @@
 #Time Complexity: O(n²), as each element is popped and inserted at the bottom (O(n) per element).
 
 #Space Complexity: O(n), as only the recursion stack is used.
+
 #Read the stack elements from the user.
 #Pop the top element from the stack recursively until the stack becomes empty.
 #While returning from recursion, insert each popped element at the bottom of the stack.
 #Continue until all elements are inserted at the bottom in reverse order.
-
 
 def insert_bottom(stack, temp):
     if not stack:
