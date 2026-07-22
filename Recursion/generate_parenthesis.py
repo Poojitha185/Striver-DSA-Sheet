1e905ab9c4440b5ecd1e0619b79b05a99c9f0959
Poojitha185 <poojithap741@gmail.com>
2026-07-22 20:00:00 +0530

    Day113-solving Recursion subsequences pattern problem1(using brutal force) approach

---
+++ Recursion/generate_parenthesis.py
@@ -0,0 +1,26 @@
+def is_valid(s):
+    balance=0
+    for c in s:
+        if c=='(':
+            balance+=1
+        elif c==')':
+            balance-=1
+        if balance<0:
+            return False
+    return balance==0
+def generate_all(curr,n,result):
+    if len(curr) == 2*n:
+        if is_valid(curr):
+             result.append(curr)
+        return
+    generate_all(curr + '(', n, result)
+    generate_all(curr + ')', n, result)
+def generate_parenthesis(n):
+    result=[]
+    generate_all("",n,result)
+    return result
+n=int(input("enter the n value: "))
+print("The generated parenthesis are:")
+res=generate_parenthesis(n)
+for i in res:
+    print(i)
