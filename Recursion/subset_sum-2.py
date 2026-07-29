ac9baf33aec26c27507f06cec6a199fdfb482d30
Poojitha185 <poojithap741@gmail.com>
2026-07-29 20:00:00 +0530

    Day119-solving Recursion subsequences pattern problem5(using brute force approach)

--- Recursion/subset_sum-2.py
+++ Recursion/subset_sum-2.py
@@ -1,5 +1,6 @@
 #Time Complexity: O(N × 2^N) More precisely, the recursion itself generates 2ⁿ subsets, but storing/converting each subset costs up to O(n).
 #Space Complexity: O(N * 2^N). We store up to 2^N subsets in the set, each subset storing up to N elements in the worst case. Additionally, O(N) space is used for the recursion stack during subset generation.
+#To find all unique subsets from an array like [1, 2, 2], the most direct way is to first generate every possible combination of elements (subsets). We can do this with a "pick" or "don't pick" choice for each element. This process, however, will create duplicates, like generating [1, 2] twice. To solve this, we can store all the generated subsets in a special container, a Set, which automatically discards any duplicates. After generating all possibilities, we just copy the unique subsets from the Set to a list for the final answer.
 def subset(ind,curr,arr,ans):
     n=len(arr)
     if ind==n:
