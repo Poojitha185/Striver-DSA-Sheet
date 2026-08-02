0bd230e65529590fb5e0e201be4576f50dad88a8
Poojitha185 <poojithap741@gmail.com>
2026-08-02 20:00:00 +0530

    Day120-solving Recursion subsequences pattern problem5(using optimal approach)

--- Recursion/subset_sum-2.1.py
+++ Recursion/subset_sum-2.1.py
@@ -4,6 +4,10 @@
 #Instead of generating all subsets and then removing duplicates, we can avoid creating duplicates in the first place. This is done by sorting the input array first so that all duplicate numbers are adjacent. While generating subsets through backtracking, if we encounter a number that is the same as the previous one and it’s not the first in the current recursive call, we skip it. This pruning step ensures we only generate unique subsets without extra storage for duplicate removal.
 
 #Sorting is essential here because without sorting, duplicates would be scattered and hard to skip correctly. This method is efficient and avoids unnecessary subset generation, making it better in both runtime and memory usage compared to the brute force approach.
+#Sort the input array so that duplicates are adjacent.
+#Initialize a list to store the current subset and a list of lists to store all unique subsets.Use a recursive backtracking function that: Adds the current subset to the list of results.Iterates from the current index to the end of the array.
+#If the current element is the same as the previous one and not at the starting index of this recursion, skip it. Include the current element in the subset and recurse for the next index.
+#Backtrack by removing the last added element.
 def subset(ind,curr,arr,ans):
     n=len(arr)
     ans.append(list(curr))
