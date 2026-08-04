7fcaa70dd91f3afc1ec4d871e159e4a28d4978ea
Poojitha185 <poojithap741@gmail.com>
2026-08-04 20:00:00 +0530

    Day121-solving Recursion subsequences pattern problem6(using recursion and backtracking)

--- Recursion/letter_combination_phone_number.py
+++ Recursion/letter_combination_phone_number.py
@@ -1,38 +1,31 @@
 #Time Complexity: O(4^N * N), where n is the length of the input digits. This is because each digit can map to up to 4 letters, and there are n digits.
 
 #Space Complexity: O(N), where n is the length of the input digits. This is due to the recursion stack depth.
-class Solution:
-    def __init__(self):
-        # Mapping digits to corresponding characters
-        self.map = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
-
-    # Recursive helper function to generate combinations
-    def helper(self, digits, ans, index, current):
+# Recursive helper function to generate combinations
+def helper(digits, ans, index, current):
+        k=["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         # Base case: if index reaches the end of digits
         if index == len(digits):
             # Add the current combination to the answer
             ans.append(current)
             return
         # Get characters corresponding to the current digit
-        s = self.map[int(digits[index])]
+        s = k[int(digits[index])]
         # Loop through the corresponding characters
         for char in s:
             # Recursively call function with next index
             # Add current character to the string
-            self.helper(digits, ans, index + 1, current + char)
+            helper(digits, ans, index + 1, current + char)
 
     # Function to get all letter combinations for a given digit string
-    def letterCombinations(self, digits):
+def letterCombinations(digits):
         ans = []  # List to store results
         # Return empty list if digits string is empty
         if not digits:
             return ans
         # Initiate recursive function
-        self.helper(digits, ans, 0, "")
+        helper(digits, ans, 0, "")
         return ans  # Return the result
-if __name__ == "__main__":
-    solution = Solution()
-    digits = "23"  # Input digits
-    result = solution.letterCombinations(digits)  # Get combinations
-    # Print the results
-    print(result)
+digits=input("enter the digits: ")
+result=letterCombinations(digits)
+print(result)
