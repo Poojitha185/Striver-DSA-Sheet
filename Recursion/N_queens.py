99f9ae0ffad4ac83ddb23aeea9cd9aaff71c7cf6
Poojitha185 <poojithap741@gmail.com>
2026-08-07 20:00:00 +0530

    Day122-solving Recursion Trying out all combos/Hard problem1(using brute force approach)

--- Recursion/N_queens.py
+++ Recursion/N_queens.py
@@ -1,6 +1,8 @@
 #Time Complexity: O(N!*N), we try all possible permutations of placing the queens and check for safety.
 #Space Complexity: O(N^2 + N), additional space used for storing distinct boards and stack space.
+#We use backtracking to explore all possible board configurations by placing one queen per column. For each column, we try placing a queen in every row, checking whether the position is safe (i.e., not attacked by any queen placed in previous columns).
 
+#If a position is safe, we place the queen and recursively attempt to place queens in the next column. If we reach a point where no row in a column is safe, we backtrack and remove the previously placed queen and try a different row. This continues until all queens are placed successfully or all configurations have been tried.
 def issafe(row,col,n,board):
     r=row
     c=col
