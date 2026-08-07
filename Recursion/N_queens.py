430ecd6e4ce0cf46f32271706066b9e8ca226522
Poojitha185 <poojithap741@gmail.com>
2026-08-07 20:00:00 +0530

    Day122-solving Recursion Trying out all combos/Hard problem1(using brute force approach)

--- Recursion/N_queens.py
+++ Recursion/N_queens.py
@@ -1,67 +1,47 @@
-#Time Complexity: O(N!*N), we try all possible permutations of placing the queens and check for safety.
-#Space Complexity: O(N^2 + N), additional space used for storing distinct boards and stack space.
-#We use backtracking to explore all possible board configurations by placing one queen per column. For each column, we try placing a queen in every row, checking whether the position is safe (i.e., not attacked by any queen placed in previous columns).
-#If a position is safe, we place the queen and recursively attempt to place queens in the next column. If we reach a point where no row in a column is safe, we backtrack and remove the previously placed queen and try a different row. This continues until all queens are placed successfully or all configurations have been tried.
+def issafe(row,col,n,board):
+    r=row
+    c=col
+    while(r>=0 and c>=0):
+        if board[r][c]=="Q":
+            return False
+        r-=1
+        c-=1
+    r=row
+    c=col
+    while(c>=0):
+        if board[r][c]=='Q':
+            return False
+        c-=1
+    r=row
+    c=col
+    while(r<n and c>=0):
+        if board[r][c]=="Q":
+            return False
+        r+=1
+        c-=1
+    return True
+def solve(col,board,ans,n):
+    if col == n:
+        ans.append([row[:] for row in board])
+        return
+    for i in range(n):
+        if(issafe(i,col,n,board)):
+            board[i][col]="Q"
+            solve(col+1,board,ans,n)
+            board[i][col]="."
+def Nqueens(n):
+    board = []
+    for i in range(n):
+       row = []
+       for j in range(n):
+           row.append(".")
+       board.append(row)
+    ans=[]
+    solve(0,board,ans,n)
+    return ans
+n=int(input("enter the value of n: "))
+ans=Nqueens(n)
+for i in ans:
+    for j in i:
+        print(j)
 
-class Solution:
-    # Function to check if placing a queen is safe
-    def isSafe(self, row, col, board, n):
-        # Check left in same row
-        for j in range(col):
-            if board[row][j] == 'Q':
-                return False
-        # Check upper-left diagonal
-        i, j = row, col
-        while i >= 0 and j >= 0:
-            if board[i][j] == 'Q':
-                return False
-            i -= 1
-            j -= 1
-        # Check lower-left diagonal
-        i, j = row, col
-        while i < n and j >= 0:
-            if board[i][j] == 'Q':
-                return False
-            i += 1
-            j -= 1
-
-        # Return true if safe
-        return True
-
-    # Backtracking function
-    def solve(self, col, board, ans, n):
-        # If all columns are filled
-        if col == n:
-            temp = [''.join(row) for row in board]
-            ans.append(temp)
-            return
-
-        # Try placing in each row
-        for row in range(n):
-            if self.isSafe(row, col, board, n):
-                # Place queen
-                board[row][col] = 'Q'       
-                # Recurse
-                self.solve(col + 1, board, ans, n)  
-                # Backtrack
-                board[row][col] = '.'       
-    # Main function
-    def solveNQueens(self, n):
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        ans = []
-        self.solve(0, board, ans, n)
-        return ans
-# Driver code
-if __name__ == "__main__":
-    # Create object
-    obj = Solution()
-    # Set size
-    n = 4
-    # Solve
-    res = obj.solveNQueens(n)
-
-    # Print solutions
-    for board in res:
-        for row in board:
-            print(row)
-        print()
