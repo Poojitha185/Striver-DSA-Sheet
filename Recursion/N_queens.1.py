6987951d437eb2fbdcc903d76945f0a08482782e
Poojitha185 <poojithap741@gmail.com>
2026-08-09 19:13:00 +0530

    Day123-solving Recursion Trying out all combos/Hard problem1(using optimal(using formula) approach)

--- Recursion/N_queens.1.py
+++ Recursion/N_queens.1.py
@@ -1,43 +1,32 @@
-
-#Time Complexity: O(N!), we try all possible permutations of placing the queens.
-#Space Complexity: O(N), three boolean arrays are stored to check for safety.
-
-class Solution:
-    # Function to solve N-Queens
-    def solve(self, col, board, n, leftRow, upperDiagonal, lowerDiagonal, ans):
-        # If all queens are placed
-        if col == n:
-            ans.append(["".join(row) for row in board])
-            return
-
-        # Iterate through rows
-        for row in range(n):
-            # Check safety
-            if leftRow[row] == 0 and lowerDiagonal[row + col] == 0 and \
-               upperDiagonal[n - 1 + col - row] == 0:
-
-                # Place queen
-                board[row][col] = 'Q'
-                leftRow[row] = lowerDiagonal[row + col] = upperDiagonal[n - 1 + col - row] = 1
-
-                # Recurse
-                self.solve(col + 1, board, n, leftRow, upperDiagonal, lowerDiagonal, ans)
-
-                # Backtrack
-                board[row][col] = '.'
-                leftRow[row] = lowerDiagonal[row + col] = upperDiagonal[n - 1 + col - row] = 0
-    def solveNQueens(self, n):
-        ans = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        leftRow = [0] * n
-        lowerDiagonal = [0] * (2 * n - 1)
-        upperDiagonal = [0] * (2 * n - 1)
-        self.solve(0, board, n, leftRow, upperDiagonal, lowerDiagonal, ans)
-        return ans
-# Driver code
-sol = Solution()
-res = sol.solveNQueens(4)
-for board in res:
-    for row in board:
-        print(row)
-    print()
+def solve(col,board,ans,leftrow,upperdiagonal,lowerdiagonal,n):
+    if col==n:
+        ans.append([''.join(row) for row in board])
+        return 
+    for row in range(n):
+     if(leftrow[row]==0 and upperdiagonal[(n-1)+(col-row)]==0 and lowerdiagonal[col+row]==0):
+        board[row][col]="Q"
+        leftrow[row]=1
+        upperdiagonal[(n-1)+(col-row)]=1
+        lowerdiagonal[col+row]=1
+        solve(col+1,board,ans,leftrow,upperdiagonal,lowerdiagonal,n)
+        board[row][col]="."
+        leftrow[row]=0
+        upperdiagonal[(n-1)+(col-row)]=0
+        lowerdiagonal[col+row]=0
+def Nqueens(n):
+    board=[]
+    for i in range(n):
+      row=[]
+      for j in range(n):
+        row.append(".")
+      board.append(row)
+    ans=[]
+    leftrow = [0] * n                      #it will create list with all indexes containing value as zero It won't cause an index error because the size 2*n - 1 is chosen specifically to cover all possible diagonal indices.
+    upperdiagonal = [0] * (2 * n - 1)
+    lowerdiagonal = [0] * (2 * n - 1)
+    solve(0,board,ans,leftrow,upperdiagonal,lowerdiagonal,n)
+    return ans
+n=int(input("enter the value of n: "))
+ans=Nqueens(n)
+for i in ans:
+   print(i)
