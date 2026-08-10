f54a8dd6909ccc59da04d979492378b3ae046e53
Poojitha185 <poojithap741@gmail.com>
2026-08-10 20:00:00 +0530

    Day124-solving Recursion Trying out all combos/Hard problem2 of finding mincost

---
+++ Recursion/palindrome_partitioning_mincost.py
@@ -0,0 +1,19 @@
+def palindrome_partitoning(i,n,s):
+    if i==n:
+        return 0
+    min_cost=float('inf')
+    for j in range(i,n):
+        if(ispalindrome(i,j,s)):
+           cost=1+palindrome_partitoning(j+1,n,s)
+           min_cost=min(cost,min_cost)
+    return min_cost
+def ispalindrome(i,j,s):
+    while(i<j):
+        if s[i]!=s[j]:
+           return False
+        i=i+1
+        j=j-1
+    return True
+s=input("enter the string: ")
+n=len(s)
+print(palindrome_partitoning(0,n,s)-1)
