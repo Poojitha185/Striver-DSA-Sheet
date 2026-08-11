0ab971140dfca05302b2185bbab207b47ce619b9
Poojitha185 <poojithap741@gmail.com>
2026-08-11 20:48:09 +0530

    Day125-solving Recursion Trying out all combos/Hard problem2(using optimal approach)

---
+++ Recursion/palindrome_partitioning.py
@@ -0,0 +1,26 @@
+#TC:number of recursive states/partitions × choices per state × palindrome checking = 2ⁿ × n × n.
+#SC:Your space complexity remains O(n) because of the recursion stack.
+
+def palindrome_partitoning(i,n,s,ans,cur):
+    if i==n:
+        ans.append(cur[:])     #cu[:]->copy the entire list
+        return 
+    for j in range(i,n):
+        if(ispalindrome(i,j,s)):
+           cur.append(s[i:j+1])
+           palindrome_partitoning(j+1,n,s,ans,cur)
+           cur.pop()
+def ispalindrome(i,j,s):
+    while(i<j):
+        if s[i]!=s[j]:
+           return False
+        i=i+1
+        j=j-1
+    return True
+
+s=input("enter the string: ")
+ans=[]
+cur=[]
+n=len(s)
+palindrome_partitoning(0,n,s,ans,cur)
+print(ans)
