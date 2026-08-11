2035201c8490a4a4cf160b2b8bd06f0b3b0ce38e
Poojitha185 <poojithap741@gmail.com>
2026-08-11 20:56:56 +0530

    Day125-solving Recursion Trying out all combos/Hard problem2(using optimal approach)

--- Recursion/palindrome_partitioning.py
+++ Recursion/palindrome_partitioning.py
@@ -1,7 +1,12 @@
 #Time Complexity:  O(N² × 2ᴺ) There can be up to 2^(N-1) possible partitions.For every possible substring, ispalindrome() can take O(N) in the worst case.At the base case:ans.append(cur[:])you copy the current partition, which can contain up to N strings, costing up to O(N).
 #Space Complexity: O(N × 2ᴺ)  Because you store all possible palindrome partitions in ans.There can be exponentially many partitions, and each partition can contain up to N strings.Number of partitions → O(2ᴺ)Size of each partition → O(N)
 #Auxiliary Space: O(N)        ← recursion + current partition
-
+# 1.Start from index i and try all possible ending positions j.
+# 2.Check whether the substring s[i:j+1] is a palindrome.
+# 3.If it is a palindrome, add the substring to cur.
+# 4.Recursively partition the remaining string from j+1.
+# 5.If i reaches n, add a copy of cur to ans.
+# 6.Backtrack by removing the last added substring from cur.
 def palindrome_partitoning(i,n,s,ans,cur):
     if i==n:
         ans.append(cur[:])     #cu[:]->copy the entire list
