3a7526456f4690390728d6f406d9d2d62fae2a5d
Poojitha185 <poojithap741@gmail.com>
2026-08-14 20:00:00 +0530

    Day127-solving Recursion Trying out all combos/Hard problem4 using optimal(recursion and backtracing) approach

--- Recursion/sudoku_solver.py
+++ Recursion/sudoku_solver.py
@@ -3,6 +3,7 @@
 #Space Complexity: O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
 
 #Since we have to fill the empty cells with available possible numbers and we can also have multiple solutions, the main intuition is to try every possible way of filling the empty cells. And the more correct way to try all possible solutions is to use recursion. In each call to the recursive function, we just try all the possible numbers for a particular cell and transfer the updated board to the next recursive call.
+
 def solve(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
@@ -16,6 +17,7 @@ def solve(board):
                            board[i][j]="."
                 return False
     return True
+
 def isvalid(board,row,col,c):
     for i in range(9):
         if(board[i][col]==c):
@@ -26,7 +28,8 @@ def isvalid(board,row,col,c):
                                                                    #(i//3) to get quotient bcz quotient remains same till 3 elements 
             return False                                           #(i%3) to get remainderit changes every single time it is taken bcz column for ever single element it changes every single time
     return True
-import ast
+
+import ast                                                          # #You can convert the entered string into a Python list using ast.literal_eval()
 board = ast.literal_eval(input("Enter board: "))  
 (solve(board))
 for row in board:
