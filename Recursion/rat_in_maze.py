c5ed42e793cb3c02afba7a08eb161b92025513fb
Poojitha185 <poojithap741@gmail.com>
2026-08-15 20:00:00 +0530

    Day128-solving Recursion Trying out all combos/Hard problem5 using optimal(using recursion and backtracking with if conditions) approach

--- Recursion/rat_in_maze.py
+++ Recursion/rat_in_maze.py
@@ -1,5 +1,9 @@
 #Time Complexity: O(4^(N*N)),, because on every cell we need to try 4 different directions.
+
 #Space Complexity: O(N*N), additional space for visited array and maximum Depth of the recursion tree(auxiliary space).
+
+#Since the rat need to find all the paths to reach destination it should explore the maze step by step, trying every possible direction at each junction. If it reaches a dead end or an already visited cell, it backs up to the last decision point and tries another route. This way, it explores all possible paths from the start to the destination without missing any options. By marking cells as visited when moving forward and unmarking them when stepping back, the rat avoids infinite loops and ensures every path is explored exactly once.
+
 def solve(i,j,n,maze,visited,ans,move):
     if(i==n-1 and j==n-1):
         ans.append(move)
@@ -24,6 +28,7 @@ def solve(i,j,n,maze,visited,ans,move):
           visited[i][j]=1
           solve(i-1,j,n,maze,visited,ans,move+"U")
           visited[i][j]=0
+          
 import ast
 maze = ast.literal_eval(input("Enter the maze: "))       #is used because when you type a 2D list through input(), Python initially receives it as a string.   ast.literal_eval(...) takes that string and converts it into the corresponding Python data structure.
 n=len(maze)
