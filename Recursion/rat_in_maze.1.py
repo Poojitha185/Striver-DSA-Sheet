a690122b570ce459a280e10105aba13b313072e5
Poojitha185 <poojithap741@gmail.com>
2026-08-16 20:00:00 +0530

    Day129-solving Recursion Trying out all combos/Hard problem5 using optimal(using recursion and backtracking with single for loop) approach

--- Recursion/rat_in_maze.1.py
+++ Recursion/rat_in_maze.1.py
@@ -1,3 +1,9 @@
+#Time Complexity: O(4^(N*N)),, because on every cell we need to try 4 different directions.
+
+#Space Complexity: O(N*N), additional space for visited array and maximum Depth of the recursion tree(auxiliary space).
+
+#when they give directions more than 4 for example 8 then we have to use more if conditions like one condition for each direction thats why we created two di and dj arrays with direction values and using one single for loop we can handle all 4 directions here 
+
 def solve(i,j,n,maze,visited,ans,move,di,dj):
     if(i==n-1 and j==n-1):                        #base case
         ans.append(move)
