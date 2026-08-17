d15e3f0952398613fa1c98298d1570bbb96aa231
Poojitha185 <poojithap741@gmail.com>
2026-08-17 20:00:00 +0530

    Day129-solving Recursion Trying out all combos/Hard problem5 using optimal(using recursion and backtracking with single for loop) approach

--- Recursion/rat_in_maze.1.py
+++ Recursion/rat_in_maze.1.py
@@ -20,11 +20,11 @@ def solve(i,j,n,maze,visited,ans,move,di,dj):
             solve(nexti,nextj,n,maze,visited,ans,move+direction[ind],di,dj)     # direction[ind] stores the corresponding direction character.# nexti and nextj represent the cell we are ABOUT TO MOVE INTO.
             visited[i][j]=0                                                     # Backtarcking
 import ast
-maze = ast.literal_eval(input("Enter the maze: "))                            #is used because when you type a 2D list through input(), Python initially receives it as a string.   ast.literal_eval(...) takes that string and converts it into the corresponding Python data structure.
+maze = ast.literal_eval(input("Enter the maze: "))                              #is used because when you type a 2D list through input(), Python initially receives it as a string.   ast.literal_eval(...) takes that string and converts it into the corresponding Python data structure.
 n=len(maze)
 visited = [[0 for _ in range(n)] for _ in range(n)]
 ans=[]
-di=[1,0,0,-1]                                                                 #to get directions we have to add these values to i and j
+di=[1,0,0,-1]                                                                   #to get directions we have to add these values to i and j
 dj=[0,-1,1,0]
 if maze[0][0]==1:
    solve(0,0,n,maze,visited,ans,"",di,dj)
