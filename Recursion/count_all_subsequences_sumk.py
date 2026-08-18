ff57fcb49ba85598b2f0ea263a94a710eeeb74cb
Poojitha185 <poojithap741@gmail.com>
2026-08-18 20:00:00 +0530

    Day130-solving Recursion subsequences pattern problem7(using recursion)

--- Recursion/count_all_subsequences_sumk.py
+++ Recursion/count_all_subsequences_sumk.py
@@ -5,6 +5,7 @@
 ##Use recursion to explore all combinations of elements that could contribute to the target sum.
 #At each step, decide whether to include or exclude the current element.
 #Sum the results of both choices — inclusion and exclusion — to get the total number of valid subsets.
+
 def countSubsequenceWithTargetSum(ind, sum, nums):
         # Base case: if sum is 0, one valid subsequence is found
         if sum == 0:
@@ -14,8 +15,8 @@ def countSubsequenceWithTargetSum(ind, sum, nums):
             return 0
         #Recurse by including current number or excluding it from the sum
         return countSubsequenceWithTargetSum(ind + 1, sum - nums[ind], nums) + countSubsequenceWithTargetSum(ind + 1, sum, nums)      #including and not including the current number in the subsequence
-import ast
 
+import ast
 nums = ast.literal_eval(input("Enter the array: "))
 target = int(input("Enter the target sum: "))
 print("The number of subsequences with target sum is:", countSubsequenceWithTargetSum(0, target, nums))
