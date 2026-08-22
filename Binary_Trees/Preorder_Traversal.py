996cb133214f82aa6c2750326edf56fcd16e159b
Poojitha185 <poojithap741@gmail.com>
2026-08-22 20:00:00 +0530

    Day133-Solving Binary Trees Traversals problem2

---
+++ Binary_Trees/Preorder_Traversal.py
@@ -0,0 +1,26 @@
+class node:                   #creates a blueprint/template for a tree node.
+    def __init__(self,data):  #__init__ is a special Python method that runs automatically when you create an object.You could technically use another method, but then you'd have to call it yourself. __init__ is convenient because Python calls it automatically when the object is created.
+        self.data=data        #self means the current Node object.
+        self.left=None        #None simply means there is currently no child there.In Python, None is basically the equivalent of null in languages like C, C++, Java, and JavaScript.
+        self.right=None
+def create_tree():
+    data = int(input("Enter data (-1 for no node): "))
+    if data == -1:
+        return None
+    root = node(data)
+    print("Enter left child of", data)
+    root.left = create_tree()
+    print("Enter right child of", data)
+    root.right = create_tree()
+    return root
+def preorder_traversal(node):
+    if node==None:                  #After creating the tree, a missing child is None, not -1.
+        return
+    print(node.data)
+    preorder_traversal(node.left)
+    preorder_traversal(node.right)
+
+# Create the tree
+root = create_tree()
+# Find traversals
+preorder_traversal(root)
