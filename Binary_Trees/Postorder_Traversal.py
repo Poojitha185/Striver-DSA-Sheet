24d3367c5dbdf3adb7592efb9cc64b20f0df7381
Poojitha185 <poojithap741@gmail.com>
2026-08-24 20:00:00 +0530

    Day135-Solving Binary Trees Traversals problem4(using Iterative(using two stacks) approach)

--- Binary_Trees/Postorder_Traversal.py
+++ Binary_Trees/Postorder_Traversal.py
@@ -1,5 +1,13 @@
 #Time Complexity: O(N), where N is the number of nodes in the binary tree. Each node is visited once during the traversal.
 #Space Complexity: O(2N),The space is used by the 2 stacks to store nodes during traversal.
+#Create two stacks: one for holding nodes and another for storing the final postorder traversal sequence. Initialize an array `postorder` to store the traversal sequence.
+#Push the root node to the first stack.#Process the nodes until the first stack is empty:#Pop a node from the top of the first stack.
+#Push this node onto the second stack.
+#Push its left child (if it exists) onto the first stack.
+#Push its right child (if it exists) onto the first stack.
+#Once the first stack is empty, retrieve the nodes in the postorder sequence by popping nodes from the second stack one by one and store them in the postorder array.
+
+
 class node:                   #creates a blueprint/template for a tree node.
     def __init__(self,data):  #__init__ is a special Python method that runs automatically when you create an object.You could technically use another method, but then you'd have to call it yourself. __init__ is convenient because Python calls it automatically when the object is created.
         self.data=data        #self means the current Node object.
