2804c455ae1da5ed567fa399aa73ed56d7255435
Poojitha185 <poojithap741@gmail.com>
2026-08-25 20:00:00 +0530

    Day136-Solving Binary Trees Traversals problem5(using Iterative approach)

--- Binary_Trees/Iterative_preorder_traversal.py
+++ Binary_Trees/Iterative_preorder_traversal.py
@@ -2,6 +2,8 @@
 
 #Space Complexity: O(H), where H is the height of the binary tree. The space is used by the stack to store nodes during traversal.
 
+#iterative approach using stack to traverse the binary tree in preorder (root, left, right) manner. The algorithm uses a stack to keep track of nodes to be processed. It starts by pushing the root node onto the stack. Then, it enters a loop that continues until the stack is empty. In each iteration, it pops a node from the stack, processes it (adds its value to the result), and pushes its right child followed by its left child onto the stack (if they exist). This ensures that the left child is processed before the right child, maintaining the preorder traversal order.
+
 class node:                   #creates a blueprint/template for a tree node.
     def __init__(self,data):  #__init__ is a special Python method that runs automatically when you create an object.You could technically use another method, but then you'd have to call it yourself. __init__ is convenient because Python calls it automatically when the object is created.
         self.data=data        #self means the current Node object.
