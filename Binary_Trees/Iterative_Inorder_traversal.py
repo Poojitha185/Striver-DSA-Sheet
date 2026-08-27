b16a92632458aafa067db687908e70c64cbb935b
Poojitha185 <poojithap741@gmail.com>
2026-08-27 20:00:00 +0530

    Day137-Solving Binary Trees Traversals problem6(using Iterative approach)

--- Binary_Trees/Iterative_Inorder_traversal.py
+++ Binary_Trees/Iterative_Inorder_traversal.py
@@ -1,3 +1,7 @@
+#Time Complexity: O(n), where n is the number of nodes in the binary tree. Each node is visited exactly once.
+
+#Space Complexity: O(h), where h is the height of the binary tree. This is the space required for the stack to store the nodes during traversal.
+
 class node:                   #creates a blueprint/template for a tree node.
     def __init__(self,data):  #__init__ is a special Python method that runs automatically when you create an object.You could technically use another method, but then you'd have to call it yourself. __init__ is convenient because Python calls it automatically when the object is created.
         self.data=data        #self means the current Node object.
