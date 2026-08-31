bc827963eb8ad945c2ba6988ff0ba1abd72cf04d
Poojitha185 <poojithap741@gmail.com>
2026-08-31 20:00:00 +0530

    Day140-Solving Binary Trees Medium problems problem1(using level order Travesral  approach)

--- Binary_Trees/maximum_depth_binary_tree.1.py
+++ Binary_Trees/maximum_depth_binary_tree.1.py
@@ -1,6 +1,11 @@
 #Time Complexity: O(N), each node is processed once in Level Order Traversal.
 #Space Complexity: O(N), in worst case, a maximum of N/2 nodes can be present in queue.
 #To find the depth (or height) of a binary tree using BFS, we can take advantage of level-order traversal. Since each level of the tree corresponds to one unit of depth, we can traverse the tree level by level, and the number of levels we visit gives us the depth.
+#Begin a loop that continues until the queue becomes empty where at each level:
+#Increment `level` by 1, indicating we are moving to the next level.
+#Determine the number of nodes at the current level by storing the size of the queue.
+#Iterate over the number of nodes equal to the size of the queue and at each node, Pop it from front of the queue and push its left and right children (if they exist).
+
 class node:                   
     def __init__(self,data):    
         self.data=data        
@@ -17,6 +22,7 @@ def create_tree():
     print("Enter right child of", data)
     root.right = create_tree()
     return root
+
 from queue import Queue
 def max_depth(root):
     if root is None:
@@ -34,5 +40,6 @@ def max_depth(root):
                 q.put(front.right) # Enqueue right child if exists
         level += 1
     return level
+
 root=create_tree()
 print("Maximum depth of the binary tree is:", max_depth(root))
