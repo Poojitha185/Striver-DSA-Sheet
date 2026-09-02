21a0550e353a20a3c4a9993439cb7012077dba16
Poojitha185 <poojithap741@gmail.com>
2026-09-02 20:00:00 +0530

    Day142-Solving Binary Trees Medium problems problem2(using brute force(using recursion) approach)

---
+++ Binary_Trees/check_balanced_binary_tree.1.py
@@ -0,0 +1,45 @@
+class node:                   
+    def __init__(self,data):    
+        self.data=data        
+        self.left=None         
+        self.right=None
+
+def create_tree():
+    data = int(input("Enter data (-1 for no node): "))
+    if data == -1:
+        return None
+    root = node(data)
+    print("Enter left child of", data)
+    root.left = create_tree()
+    print("Enter right child of", data)
+    root.right = create_tree()
+    return root
+
+def isBalanced( root):
+        # If the tree is empty, it's balanced
+        if root is None:
+            return True
+
+        # Calculate the height of left and right subtrees
+        leftHeight = getHeight(root.left)
+        rightHeight = getHeight(root.right)
+
+        # Check if the absolute difference in heights of left and right subtrees is <= 1
+        if abs(leftHeight - rightHeight) <= 1 and isBalanced(root.left) and isBalanced(root.right):         #abs() is a built-in Python function that returns the absolute value of a number. In simple words: it removes the negative sign.
+            return True
+        # If any condition fails, the tree is unbalanced
+        return False
+    # Function to calculate the height of a subtree
+def getHeight(root):
+        # Base case: if the current node is NULL, return 0 (height of an empty tree)
+        if root is None:
+            return 0
+
+        # Recursively calculate the height of left and right subtrees
+        leftHeight = getHeight(root.left)
+        rightHeight = getHeight(root.right)
+
+        # Return the maximum height of left and right subtrees plus 1 (for the current node)
+        return max(leftHeight, rightHeight) + 1
+root=create_tree()
+print("is the binary tree balanced?",isBalanced(root) )
