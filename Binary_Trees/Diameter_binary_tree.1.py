5df610d2f622f7a1cac2b9ca8fd4598a45a1549b
Poojitha185 <poojithap741@gmail.com>
2026-09-05 21:50:38 +0530

    Day144-Solving Binary Trees Medium problems problem3(using optimal approach)

--- Binary_Trees/Diameter_binary_tree.1.py
+++ Binary_Trees/Diameter_binary_tree.1.py
@@ -1,6 +1,12 @@
 #Time Complexity: O(N) where N is the number of nodes in the Binary Tree. This complexity arises from visiting each node exactly once during the postorder traversal.
-
 #Space Complexity : O(H), where H is the height of the binary tree due to the recursion call stack (O(log N) for a balanced tree and O(N) for a skewed tree).
+#The earlier method took extra time because it kept recalculating the depth of subtrees again and again. This repetition made the solution slower, especially for larger trees. To make it faster, we can work from the bottom of the tree upwards, calculating heights only once.
+#Using a bottom-up approach called postorder traversal, we first finish work on the lower parts (left and right branches) before looking at the top (parent). This lets us measure the depth of each part and also check the diameter while going up the tree, all in one go.
+#Start by keeping a place to store the largest diameter found. Make a function that, for each point, does the following: If the point doesn't exist, say the depth is zero. Otherwise, do the following:
+
+#Measure how deep the left part goes, then do the same for the right part. Add the two depths and 1 (for the current point) to get the width at this point. Compare this width with the largest one we've seen so far and update it if it's bigger.
+
+
 class node:                   
     def __init__(self,data):    
         self.data=data        
