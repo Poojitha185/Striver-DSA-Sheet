d3176c2d065939d566e5ac24e9268d9870d9a9bf
Poojitha185 <poojithap741@gmail.com>
2026-09-07 20:00:00 +0530

    Day146-Solving Binary Trees Medium problems problem5(using queue data structure)

--- Binary_Trees/zig_zag_or_spiral_traversal.py
+++ Binary_Trees/zig_zag_or_spiral_traversal.py
@@ -1,6 +1,11 @@
 #Time Complexity: O(N) where N is the number of nodes in the binary tree. Each node of the binary tree is enqueued and dequeued exactly once, hence all nodes need to be processed and visited. Processing each node takes constant time operations which contributes to the overall linear time complexity.
-
 #Space Complexity: O(N) where N is the number of nodes in the binary tree. In the worst case, the queue has to hold all the nodes of the last level of the binary tree, the last level could at most hold N/2 nodes hence the space complexity of the queue is proportional to O(N). The resultant vector answer also stores the values of the nodes level by level and hence contains all the nodes of the tree contributing to O(N) space as well.
+
+#Zigzag traversal is a modification of the traditional level order traversal in a binary tree. Level Order Traversal explores does at each level from left or right but zigzag traversal adds a twist by alternating the direction of exploration.
+
+#At odd levels, we proceed from left to right but for even levels the order is reversed, from right to left. This is achieved by introducing a `leftToRight` flag which controls the order in which nodes are processed at each level.
+
+#When `leftToRight` is true, nodes are inserted into the level vector from left to right and when its false, nodes are inserted right to left.
 class node:                   
     def __init__(self,data):    
         self.data=data        
