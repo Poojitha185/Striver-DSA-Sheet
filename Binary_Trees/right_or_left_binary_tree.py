68b76b617bf4de8b382233ecf260799fcae8e9e3
Poojitha185 <poojithap741@gmail.com>
2026-09-10 20:00:00 +0530

    Day148-Solving Binary Trees Medium problems problem8 using brute force approach

--- Binary_Trees/right_or_left_binary_tree.py
+++ Binary_Trees/right_or_left_binary_tree.py
@@ -1,6 +1,7 @@
 #Time Complexity: O(N) where N is the number of nodes in the binary tree. Each node of the binary tree is enqueued and dequeued exactly once, hence all nodes need to be processed and visited. Processing each node takes constant time operations which contributes to the overall linear time complexity.
-
 #Space Complexity : O(N) where N is the number of nodes in the binary tree. In the worst case, the queue has to hold all the nodes of the last level of the binary tree, the last level could at most hold N/2 nodes hence the space complexity of the queue is proportional to O(N). The resultant vector answer also stores the values of the nodes level by level and hence contains all the nodes of the tree contributing to O(N) space as well.
+
+#To get the left and right views of a binary tree, we use level order traversal (BFS). We queue nodes level by level. For each level, we record all node values in order. The left view is formed by picking the first node of each level, and the right view by picking the last node of each level.
 
 from collections import deque
 
