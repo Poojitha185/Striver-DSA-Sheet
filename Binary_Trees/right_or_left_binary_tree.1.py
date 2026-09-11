7a42a9bf3a8b1ad05841ac5ce050c88539e77207
Poojitha185 <poojithap741@gmail.com>
2026-09-11 20:00:00 +0530

    Day149-Solving Binary Trees Medium problems problem8 using optimal approach

--- Binary_Trees/right_or_left_binary_tree.1.py
+++ Binary_Trees/right_or_left_binary_tree.1.py
@@ -1,4 +1,7 @@
-# TreeNode class for binary tree nodes
+#Time Complexity: O(N) In the worst case, we may visit every node in the binary tree exactly once. This happens when the tree is skewed (i.e., every node has only one child), effectively forming a linear structure. Hence, the time complexity becomes O(N), where N is the total number of nodes in the tree.
+
+#Space Complexity: O(H),The space complexity depends on the height (H) of the binary tree due to the recursion stack in depth-first traversal (like preorder, inorder, postorder). In a balanced binary tree, the height is log₂N, leading to O(log N) space. However, in the worst case (a skewed tree), the height is N, resulting in O(N) space. So the space complexity is O(H), where H is the height of the tree.
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
