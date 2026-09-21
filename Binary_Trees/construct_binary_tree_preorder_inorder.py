9326123d698a16107dc85fcb984808221ccb89af
Poojitha185 <poojithap741@gmail.com>
2026-09-21 20:00:00 +0530

    Day155-Solving Binary Trees Hard problems problem4

---
+++ Binary_Trees/construct_binary_tree_preorder_inorder.py
@@ -0,0 +1,76 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    # Builds the tree using the current inorder range.
+    def build(
+        self,
+        preorder,
+        inorder,
+        in_start,
+        in_end
+    ):
+        if in_start > in_end:
+            return None
+
+        # preorder[pre_index] is the root
+        # of the current subtree.
+        root_value = preorder[self.pre_index]
+        self.pre_index += 1
+
+        root = TreeNode(root_value)
+
+        root_index = in_start
+
+        # The root position splits inorder
+        # into left and right subtree ranges.
+        while (
+            root_index <= in_end
+            and inorder[root_index] != root_value
+        ):
+            root_index += 1
+
+        root.left = self.build(
+            preorder,
+            inorder,
+            in_start,
+            root_index - 1
+        )
+
+        root.right = self.build(
+            preorder,
+            inorder,
+            root_index + 1,
+            in_end
+        )
+
+        return root
+
+    # Reconstructs the binary tree from
+    # preorder and inorder traversals.
+    def build_tree(self, preorder, inorder):
+        self.pre_index = 0
+
+        return self.build(
+            preorder,
+            inorder,
+            0,
+            len(inorder) - 1
+        )
+
+
+if __name__ == "__main__":
+    preorder = [3, 9, 20, 15, 7]
+    inorder = [9, 3, 15, 20, 7]
+
+    solution = Solution()
+
+    root = solution.build_tree(
+        preorder,
+        inorder
+    )
